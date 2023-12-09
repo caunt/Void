d8dffc108663bb45e6eee32bd73318c1965d8962
Vitalii Kotenko <caunt.official@gmail.com>
2023-12-09 05:42:13 +0200

    update forge marker versions hint

--- src/Network/Protocol/Forge/ForgeMarker.cs
+++ src/Network/Protocol/Forge/ForgeMarker.cs
@@ -4,10 +4,10 @@ public class ForgeMarker
 {
     private static readonly List<ForgeMarker> _markers = [];
 
-    public static readonly ForgeMarker Forge = new("FORGE"); // 1.20.4
+    public static readonly ForgeMarker Forge = new("FORGE"); // 1.20.2
     public static readonly ForgeMarker Fml3 = new("FML3"); // 1.18.2
-    public static readonly ForgeMarker Fml2 = new("FML2"); // 1.13.2
-    public static readonly ForgeMarker Fml = new("FML"); // 1.8
+    public static readonly ForgeMarker Fml2 = new("FML2"); // 1.13
+    public static readonly ForgeMarker Fml = new("FML"); // 1.7.2
 
     public static ForgeMarker? Longest => _markers.MaxBy(marker => marker.Value.Length);
 
