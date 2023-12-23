eba9cd04e147652a1ccdbf927fcf75e49c10f0fe
Vitalii Kotenko <caunt.official@gmail.com>
2023-12-23 06:14:54 +0200

    allow force stoping

--- src/Void.Proxy/Program.cs
+++ src/Void.Proxy/Program.cs
@@ -3,9 +3,19 @@
 Proxy.Logger.Information("Hello, World!");
 
 var cts = new CancellationTokenSource();
+var force = false;
 
 Console.CancelKeyPress += (sender, eventArgs) =>
 {
+    if (force)
+        Environment.Exit(0);
+
+    if (cts.IsCancellationRequested)
+    {
+        Proxy.Logger.Warning("Proxy is already stopping. Press again to confirm force stop.");
+        force = true;
+    }
+
     cts.Cancel();
     eventArgs.Cancel = true;
 };
