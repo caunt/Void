040aa51ed782b59cdcf50cf1196be6094b23c417
Vitalii Kotenko <caunt.official@gmail.com>
2024-09-29 01:57:49 +0300

    fill mappings for 1.20.2->latest protocol plugin

--- src/Plugins/ProtocolSupport/Java/v1_20_2_to_latest/Registries/Mappings.cs
+++ src/Plugins/ProtocolSupport/Java/v1_20_2_to_latest/Registries/Mappings.cs
@@ -11,36 +11,40 @@ public static class Mappings
 
     public static readonly IReadOnlyDictionary<PacketMapping[], Type> ServerboundHandshakeMappings = new Dictionary<PacketMapping[], Type>
     {
-        { [new PacketMapping(0x00, ProtocolVersion.MINECRAFT_1_7_2)], typeof(HandshakePacket) }
+        { [new PacketMapping(0x00, Plugin.SupportedVersions.First())], typeof(HandshakePacket) }
     };
 
     public static readonly IReadOnlyDictionary<PacketMapping[], Type> ClientboundLoginMappings = new Dictionary<PacketMapping[], Type>
     {
-        { [new PacketMapping(0x01, ProtocolVersion.MINECRAFT_1_7_2)], typeof(EncryptionRequestPacket) },
-        { [new PacketMapping(0x03, ProtocolVersion.MINECRAFT_1_8)], typeof(SetCompressionPacket) }
+        { [new PacketMapping(0x01, Plugin.SupportedVersions.First())], typeof(EncryptionRequestPacket) },
+        { [new PacketMapping(0x03, Plugin.SupportedVersions.First())], typeof(SetCompressionPacket) }
     };
 
     public static readonly IReadOnlyDictionary<PacketMapping[], Type> ServerboundLoginMappings = new Dictionary<PacketMapping[], Type>
     {
-        { [new PacketMapping(0x01, ProtocolVersion.MINECRAFT_1_7_2)], typeof(EncryptionResponsePacket) },
-        { [new PacketMapping(0x03, ProtocolVersion.MINECRAFT_1_20_2)], typeof(LoginAcknowledgedPacket) }
+        { [new PacketMapping(0x01, Plugin.SupportedVersions.First())], typeof(EncryptionResponsePacket) },
+        { [new PacketMapping(0x03, Plugin.SupportedVersions.First())], typeof(LoginAcknowledgedPacket) }
     };
 
     public static readonly IReadOnlyDictionary<PacketMapping[], Type> ClientboundConfigurationMappings = new Dictionary<PacketMapping[], Type>
     {
-        { [new PacketMapping(0x03, ProtocolVersion.MINECRAFT_1_21)], typeof(FinishConfigurationPacket) }
+        { [new PacketMapping(0x02, Plugin.SupportedVersions.First(), ProtocolVersion.MINECRAFT_1_20_3)], typeof(FinishConfigurationPacket) },
+        { [new PacketMapping(0x03, ProtocolVersion.MINECRAFT_1_20_5)], typeof(FinishConfigurationPacket) }
     };
 
     public static readonly IReadOnlyDictionary<PacketMapping[], Type> ServerboundConfigurationMappings = new Dictionary<PacketMapping[], Type>();
 
     public static readonly IReadOnlyDictionary<PacketMapping[], Type> ClientboundPlayMappings = new Dictionary<PacketMapping[], Type>
     {
-        { [new PacketMapping(0x26, ProtocolVersion.MINECRAFT_1_21)], typeof(KeepAliveRequestPacket) }
+        { [new PacketMapping(0x24, Plugin.SupportedVersions.First(), ProtocolVersion.MINECRAFT_1_20_3)], typeof(KeepAliveRequestPacket) },
+        { [new PacketMapping(0x26, ProtocolVersion.MINECRAFT_1_20_5)], typeof(KeepAliveRequestPacket) }
     };
 
     public static readonly IReadOnlyDictionary<PacketMapping[], Type> ServerboundPlayMappings = new Dictionary<PacketMapping[], Type>
     {
-        { [new PacketMapping(0x18, ProtocolVersion.MINECRAFT_1_21)], typeof(KeepAliveResponsePacket) }
+        { [new PacketMapping(0x14, Plugin.SupportedVersions.First(), ProtocolVersion.MINECRAFT_1_20_2)], typeof(KeepAliveResponsePacket) },
+        { [new PacketMapping(0x15, ProtocolVersion.MINECRAFT_1_20_3, ProtocolVersion.MINECRAFT_1_20_3)], typeof(KeepAliveResponsePacket) },
+        { [new PacketMapping(0x18, ProtocolVersion.MINECRAFT_1_20_5)], typeof(KeepAliveResponsePacket) },
     };
 
     public static void Fill()
