d0cb1fc466066990aeda6aa35bdcada3de23ebb6
Vitalii Kotenko <caunt.official@gmail.com>
2025-03-23 12:28:14 +0200

    Enhance Docker environment configuration flexibility
    Introduced a dynamic `count` variable in `Program.cs` to replace the fixed count of `0` when starting the Docker environment. The new implementation allows users to specify the count via command-line arguments, defaulting to `2` if no arguments are provided or if parsing fails. This change improves the configurability of the Docker environment setup.

--- src/Debug/Program.cs
+++ src/Debug/Program.cs
@@ -7,7 +7,8 @@ using System.Reflection;
 using Void.Proxy.API.Mojang.Minecraft.Network;
 
 var version = ProtocolVersion.Latest;
-await StartDockerEnvironmentAsync(count: 0);
+var count = 2;
+await StartDockerEnvironmentAsync(count: args.Length is 0 ? count : int.TryParse(args[0], out count) ? count : count);
 
 return;
 
