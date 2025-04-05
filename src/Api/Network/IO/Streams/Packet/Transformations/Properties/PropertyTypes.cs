ac523057d5a050b26022b1525322f47c94760873
Vitalii Kotenko <caunt.official@gmail.com>
2025-04-05 17:08:38 +0300

    Add new data type properties to PropertyTypes class
    This commit introduces several static properties to the `PropertyTypes` class, including `Bool`, `Byte`, `Short`, `Int`, `Long`, `Float`, `Double`, `VarLong`, `Uuid`, and `Nbt`. Each property is initialized with a new instance of its respective type, enhancing the functionality of the class.

--- src/Api/Network/IO/Streams/Packet/Transformations/Properties/PropertyTypes.cs
+++ src/Api/Network/IO/Streams/Packet/Transformations/Properties/PropertyTypes.cs
@@ -4,5 +4,15 @@ namespace Void.Proxy.Api.Network.IO.Streams.Packet.Transformations.Properties;
 
 public static class PropertyTypes
 {
+    public static BoolType Bool { get; } = new();
+    public static ByteType Byte { get; } = new();
+    public static ShortType Short { get; } = new();
+    public static IntType Int { get; } = new();
+    public static LongType Long { get; } = new();
+    public static FloatType Float { get; } = new();
+    public static DoubleType Double { get; } = new();
     public static VarIntType VarInt { get; } = new();
+    public static VarLongType VarLong { get; } = new();
+    public static UuidType Uuid { get; } = new();
+    public static NbtType Nbt { get; } = new();
 }
