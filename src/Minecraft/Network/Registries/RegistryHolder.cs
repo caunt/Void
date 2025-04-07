84636a086a568ec4b4eb35863fb85824fb92df95
Vitalii Kotenko <caunt.official@gmail.com>
2025-04-08 01:02:55 +0300

    feat(registry): add IRegistryHolder interface and implementation
    Introduce a new interface `IRegistryHolder` in the `Void.Minecraft.Network.Registries` namespace.
    Create a `RegistryHolder` class in the `Void.Proxy.Plugins.Common.Network.Registries` namespace that implements the `IRegistryHolder` interface.
    Add a `using` directive to include the `Void.Minecraft.Network.Registries` namespace in the `RegistryHolder` class file.

---
+++ src/Minecraft/Network/Registries/RegistryHolder.cs
@@ -0,0 +1,3 @@
+﻿namespace Void.Minecraft.Network.Registries;
+
+public interface IRegistryHolder;
---
+++ src/Plugins/Common/Network/Registries/RegistryHolder.cs
@@ -0,0 +1,7 @@
+﻿using Void.Minecraft.Network.Registries;
+
+namespace Void.Proxy.Plugins.Common.Network.Registries;
+
+public class RegistryHolder : IRegistryHolder
+{
+}
