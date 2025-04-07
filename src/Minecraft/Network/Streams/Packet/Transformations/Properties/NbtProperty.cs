cfe3fc2b6e502df268d1e514064b6600556e765e
Vitalii Kotenko <caunt.official@gmail.com>
2025-04-07 19:25:39 +0300

    feat(nbt): add NamedNbtProperty and enhance NbtProperty
    Introduce a new record `NamedNbtProperty` that extends `NbtProperty` to support an additional boolean parameter indicating if the property is named. Modify `NbtProperty` to include a `Named` parameter with a default value of `false`, enhancing its functionality to represent both named and unnamed properties. Additionally, implement the `AsNbtTag` method for converting the property to an `NbtTag` using a `MinecraftBuffer`.
    
    Remove the folder reference for "Transformations" in `Void.Minecraft.csproj`, indicating a potential restructuring of the project.

--- src/Minecraft/Network/Streams/Packet/Transformations/Properties/NbtProperty.cs
+++ src/Minecraft/Network/Streams/Packet/Transformations/Properties/NbtProperty.cs
@@ -5,7 +5,9 @@ using Void.Minecraft.Nbt;
 
 namespace Void.Minecraft.Network.Streams.Packet.Transformations.Properties;
 
-public record NbtProperty(ReadOnlyMemory<byte> Value) : IPacketProperty<NbtProperty>
+public record NamedNbtProperty(ReadOnlyMemory<byte> Value) : NbtProperty(Value, true);
+
+public record NbtProperty(ReadOnlyMemory<byte> Value, bool Named = false) : IPacketProperty<NbtProperty>
 {
     public NbtTag AsNbtTag => new MinecraftBuffer(Value.Span).ReadTag();
 
