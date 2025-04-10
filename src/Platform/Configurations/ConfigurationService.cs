9b1e73b63d2812db3939a789ff957ed4cf72b76e
Vitalii Kotenko <caunt.official@gmail.com>
2025-04-11 01:06:00 +0300

    feat(configurations): add ConfigurationService class
    Introduces the `ConfigurationService` class in the `Void.Proxy.Configurations` namespace, implementing the `IConfigurationService` interface. The class includes a generic method `Get<TConfiguration>` that accepts an `IPlugin` parameter and returns a default value of type `TConfiguration`, constrained to implement the `IConfiguration` interface. New using directives for `NuGet.Protocol.Plugins` and `Void.Proxy.Api.Configurations` are also added to support this functionality.

---
+++ src/Platform/Configurations/ConfigurationService.cs
@@ -0,0 +1,13 @@
+﻿using NuGet.Protocol.Plugins;
+using Void.Proxy.Api.Configurations;
+using IConfiguration = Void.Proxy.Api.Configurations.IConfiguration;
+
+namespace Void.Proxy.Configurations;
+
+public class ConfigurationService : IConfigurationService
+{
+    public TConfiguration Get<TConfiguration>(IPlugin plugin) where TConfiguration : IConfiguration
+    {
+        return default!;
+    }
+}
