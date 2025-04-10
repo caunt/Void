fdf75ccbebf81e90f6abd0917fcd9309f3531eb6
Vitalii Kotenko <caunt.official@gmail.com>
2025-04-11 02:09:02 +0300

    feat(extensions): add IntExtensions class for VarInt handling
    This commit introduces a new static class `IntExtensions` in the `Void.Minecraft.Buffers.Extensions` namespace. It includes two methods: `VarIntSize`, which calculates the size of a variable-length integer, and `AsVarInt`, which converts an integer to a span of bytes in its variable-length format. The `AsVarInt` method uses a stack-allocated byte array for improved efficiency and returns a span of bytes based on the integer's value.

---
+++ src/Minecraft/Buffers/Extensions/IntExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Numerics;
+
+namespace Void.Minecraft.Buffers.Extensions;
+
+public static class IntExtensions
+{
+    public static int VarIntSize(this int value) => (BitOperations.LeadingZeroCount((uint)value | 1) - 38) * -1171 >> 13;
+
+    public static Span<byte> AsVarInt(this int value)
+    {
+        Span<byte> buffer = stackalloc byte[5];
+
+        var unsigned = (uint)value;
+        var index = 0;
+
+        do
+        {
+            var temp = (byte)(unsigned & 127);
+            unsigned >>= 7;
+
+            if (unsigned != 0)
+                temp |= 128;
+
+            buffer[index++] = temp;
+        } while (unsigned != 0);
+
+        return buffer[..index].ToArray();
+    }
+}
