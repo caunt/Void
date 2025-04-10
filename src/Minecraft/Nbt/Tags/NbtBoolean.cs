28216aec7300e82b9097bc95a57cc7d7cc7ad4c8
Vitalii Kotenko <caunt.official@gmail.com>
2025-04-10 17:53:05 +0300

    feat(nbt): add implicit conversions for NbtBoolean
    This commit introduces a new `using` directive for the `System` namespace, enabling the use of its types and methods without full qualification.
    
    Additionally, two implicit conversion operators are added: one for converting a `ByteTag` to an `NbtBoolean` using `Convert.ToBoolean`, and another for converting an `NbtBoolean` to a `NbtByte` by casting it to a `ByteTag`.

--- src/Minecraft/Nbt/Tags/NbtBoolean.cs
+++ src/Minecraft/Nbt/Tags/NbtBoolean.cs
@@ -1,10 +1,13 @@
 ﻿using SharpNBT;
+using System;
 
 namespace Void.Minecraft.Nbt.Tags;
 
 public record NbtBoolean(bool Value = false) : NbtTag
 {
     public static implicit operator ByteTag(NbtBoolean tag) => new(tag.Name, tag.Value);
+    public static implicit operator NbtBoolean(ByteTag tag) => new(Convert.ToBoolean(tag.Value)) { Name = tag.Name };
+
     public static implicit operator NbtByte(NbtBoolean tag) => (ByteTag)tag;
     public static implicit operator NbtBoolean(NbtByte tag) => (NbtBoolean)tag;
 
