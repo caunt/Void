06a65bc3634c3c5daa528ae7de14cedd5662f8b5
Vitalii Kotenko <caunt.official@gmail.com>
2025-04-10 18:03:19 +0300

    feat(nbt): add implicit conversions between NbtBoolean and NbtByte
    The `NbtBoolean` class now includes implicit conversion operators for `NbtByte`. The conversion from `NbtByte` to `NbtBoolean` creates a new instance using `Convert.ToBoolean`, preserving the `Name` property. A new conversion from `NbtBoolean` to `NbtByte` has also been added, utilizing `Convert.ToByte` while maintaining the `Name` property.
    
    In the `NbtByte` class, the `using System;` directive was added to support these conversion methods, and the implicit conversion from `NbtByte` to `NbtBoolean` was implemented, ensuring the `Name` property is preserved.

--- src/Minecraft/Nbt/Tags/NbtByte.cs
+++ src/Minecraft/Nbt/Tags/NbtByte.cs
@@ -1,4 +1,5 @@
 using SharpNBT;
+using System;
 
 namespace Void.Minecraft.Nbt.Tags;
 
@@ -6,6 +7,9 @@ public record NbtByte(byte Value = 0) : NbtTag
 {
     public static implicit operator NbtByte(ByteTag tag) => new(tag.Value) { Name = tag.Name };
     public static implicit operator ByteTag(NbtByte tag) => new(tag.Name, tag.Value);
+
+    public static implicit operator NbtBoolean(NbtByte tag) => new(Convert.ToBoolean(tag.Value)) { Name = tag.Name };
+    public static implicit operator NbtByte(NbtBoolean tag) => (ByteTag)tag;
 
     public override string ToString() => ToSnbt();
 }
