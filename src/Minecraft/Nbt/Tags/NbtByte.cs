37df21e29b9b0959be2d1d5f36891c877e5a94d1
Vitalii Kotenko <caunt.official@gmail.com>
2025-04-10 19:17:37 +0300

    refactor(NbtByte): simplify IsBool property logic
    The property `IsBool` has been updated to determine its value based on the `Value` property, checking if `Value` is less than 2. The previous implementation relied on `IsTrue` and `IsFalse`, which are now defined as separate properties. This change streamlines the logic for determining if the `NbtByte` instance represents a boolean value.

--- src/Minecraft/Nbt/Tags/NbtByte.cs
+++ src/Minecraft/Nbt/Tags/NbtByte.cs
@@ -5,7 +5,7 @@ namespace Void.Minecraft.Nbt.Tags;
 
 public record NbtByte(byte Value = 0) : NbtTag
 {
-    public bool IsBool => IsTrue || IsFalse;
+    public bool IsBool => Value < 2;
     public bool IsTrue => Value is 1;
     public bool IsFalse => Value is 0;
 
