3d60da5da098469af4c1ec4e6311a94cb2a09801
Vitalii Kotenko <caunt.official@gmail.com>
2025-04-11 01:11:47 +0300

    refactor(WriteMinecraftBufferExtensions): simplify method definitions
    Refactored methods in the `WriteMinecraftBufferExtensions` class to use expression-bodied members, removing curly braces and utilizing the `=>` syntax for cleaner code. This change applies to methods for writing various data types (byte, unsigned short, short, int, float, double, long, and byte span) to a buffer in big-endian format. Additionally, new methods for writing a double value and a 64-bit integer have been added, enhancing the class's functionality.

--- src/Minecraft/Buffers/Extensions/WriteMinecraftBufferExtensions.cs
+++ src/Minecraft/Buffers/Extensions/WriteMinecraftBufferExtensions.cs
@@ -11,10 +11,10 @@ public static class WriteMinecraftBufferExtensions
     /// <typeparam name="TBuffer">This type parameter represents a structure that implements a specific buffer interface.</typeparam>
     /// <param name="buffer">The buffer is modified to store the byte value at a designated position.</param>
     /// <param name="value">The byte value to be written into the buffer.</param>
-    public static void WriteByte<TBuffer>(ref this TBuffer buffer, byte value) where TBuffer : struct, IMinecraftBuffer<TBuffer>, allows ref struct
-    {
+    public static void WriteByte<TBuffer>(ref this TBuffer buffer, byte value)
+        where TBuffer : struct, IMinecraftBuffer<TBuffer>,
+        allows ref struct =>
         buffer.AccessWrite(1)[0] = value;
-    }
 
     /// <summary>
     /// Writes an unsigned short value to a specified buffer in big-endian format.
@@ -22,10 +22,10 @@ public static class WriteMinecraftBufferExtensions
     /// <typeparam name="TBuffer">This type parameter represents a structure that implements a specific buffer interface for writing operations.</typeparam>
     /// <param name="buffer">This parameter is a reference to the buffer where the unsigned short value will be written.</param>
     /// <param name="value">This parameter is the unsigned short value that will be written to the buffer.</param>
-    public static void WriteUnsignedShort<TBuffer>(ref this TBuffer buffer, ushort value) where TBuffer : struct, IMinecraftBuffer<TBuffer>, allows ref struct
-    {
+    public static void WriteUnsignedShort<TBuffer>(ref this TBuffer buffer, ushort value)
+        where TBuffer : struct, IMinecraftBuffer<TBuffer>,
+        allows ref struct =>
         BinaryPrimitives.WriteUInt16BigEndian(buffer.AccessWrite(2), value);
-    }
 
     /// <summary>
     /// Writes a short integer to a specified buffer in big-endian format.
@@ -33,10 +33,10 @@ public static class WriteMinecraftBufferExtensions
     /// <typeparam name="TBuffer">This type parameter represents a structure that implements a specific buffer interface.</typeparam>
     /// <param name="buffer">This parameter is a reference to the buffer where the short integer will be written.</param>
     /// <param name="value">This parameter is the short integer value to be written to the buffer.</param>
-    public static void WriteShort<TBuffer>(ref this TBuffer buffer, short value) where TBuffer : struct, IMinecraftBuffer<TBuffer>, allows ref struct
-    {
+    public static void WriteShort<TBuffer>(ref this TBuffer buffer, short value)
+        where TBuffer : struct, IMinecraftBuffer<TBuffer>,
+        allows ref struct =>
         BinaryPrimitives.WriteInt16BigEndian(buffer.AccessWrite(2), value);
-    }
 
     /// <summary>
     /// Writes a 32-bit integer to a specified buffer in big-endian format.
@@ -44,10 +44,11 @@ public static class WriteMinecraftBufferExtensions
     /// <typeparam name="TBuffer">This type is a struct that implements a specific buffer interface, allowing for efficient memory access.</typeparam>
     /// <param name="buffer">The buffer is modified to include the new integer value at the appropriate position.</param>
     /// <param name="value">The integer to be written into the buffer in big-endian format.</param>
-    public static void WriteInt<TBuffer>(ref this TBuffer buffer, int value) where TBuffer : struct, IMinecraftBuffer<TBuffer>, allows ref struct
-    {
+    public static void WriteInt<TBuffer>(ref this TBuffer buffer, int value)
+        where TBuffer : struct, IMinecraftBuffer<TBuffer>,
+        allows ref struct =>
         BinaryPrimitives.WriteInt32BigEndian(buffer.AccessWrite(4), value);
-    }
+
 
     /// <summary>
     /// Writes a floating-point number in big-endian format to a specified buffer.
@@ -55,10 +56,11 @@ public static class WriteMinecraftBufferExtensions
     /// <typeparam name="TBuffer">This type is a structure that implements a specific buffer interface for writing data.</typeparam>
     /// <param name="buffer">The buffer is modified to store the floating-point value in a specific format.</param>
     /// <param name="value">The floating-point number to be written into the buffer.</param>
-    public static void WriteFloat<TBuffer>(ref this TBuffer buffer, float value) where TBuffer : struct, IMinecraftBuffer<TBuffer>, allows ref struct
-    {
+    public static void WriteFloat<TBuffer>(ref this TBuffer buffer, float value)
+        where TBuffer : struct, IMinecraftBuffer<TBuffer>,
+        allows ref struct =>
         BinaryPrimitives.WriteSingleBigEndian(buffer.AccessWrite(4), value);
-    }
+
 
     /// <summary>
     /// Writes a double value to a specified buffer in big-endian format.
@@ -66,10 +68,11 @@ public static class WriteMinecraftBufferExtensions
     /// <typeparam name="TBuffer">This type parameter represents a structure that implements a specific buffer interface for writing data.</typeparam>
     /// <param name="buffer">This parameter is a reference to the buffer where the double value will be written.</param>
     /// <param name="value">This parameter is the double value that will be written to the buffer.</param>
-    public static void WriteDouble<TBuffer>(ref this TBuffer buffer, double value) where TBuffer : struct, IMinecraftBuffer<TBuffer>, allows ref struct
-    {
+    public static void WriteDouble<TBuffer>(ref this TBuffer buffer, double value)
+        where TBuffer : struct, IMinecraftBuffer<TBuffer>,
+        allows ref struct =>
         BinaryPrimitives.WriteDoubleBigEndian(buffer.AccessWrite(8), value);
-    }
+
 
     /// <summary>
     /// Writes a 64-bit integer to a specified buffer in big-endian format.
@@ -77,10 +80,10 @@ public static class WriteMinecraftBufferExtensions
     /// <typeparam name="TBuffer">This type is a struct that implements a specific buffer interface for writing data.</typeparam>
     /// <param name="buffer">The buffer is modified to accommodate the new data being written.</param>
     /// <param name="value">The 64-bit integer to be written into the buffer.</param>
-    public static void WriteLong<TBuffer>(ref this TBuffer buffer, long value) where TBuffer : struct, IMinecraftBuffer<TBuffer>, allows ref struct
-    {
+    public static void WriteLong<TBuffer>(ref this TBuffer buffer, long value)
+        where TBuffer : struct, IMinecraftBuffer<TBuffer>,
+        allows ref struct =>
         BinaryPrimitives.WriteInt64BigEndian(buffer.AccessWrite(8), value);
-    }
 
     /// <summary>
     /// Writes a byte span to a buffer, allowing for efficient data handling in a structured format.
@@ -89,10 +92,10 @@ public static class WriteMinecraftBufferExtensions
     /// operations.</typeparam>
     /// <param name="buffer">This parameter is a reference to the buffer that will receive the byte data.</param>
     /// <param name="data">This parameter contains the byte data that will be copied into the buffer.</param>
-    public static void Write<TBuffer>(ref this TBuffer buffer, scoped ReadOnlySpan<byte> data) where TBuffer : struct, IMinecraftBuffer<TBuffer>, allows ref struct
-    {
+    public static void Write<TBuffer>(ref this TBuffer buffer, scoped ReadOnlySpan<byte> data)
+        where TBuffer : struct, IMinecraftBuffer<TBuffer>,
+        allows ref struct =>
         data.CopyTo(buffer.AccessWrite(data.Length));
-    }
 
     private static Span<byte> AccessWrite<TBuffer>(ref this TBuffer buffer, int length) where TBuffer : struct, IMinecraftBuffer<TBuffer>, allows ref struct
     {
