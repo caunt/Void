9056c685a7e4362ca95e8a16d2f09f1042385d72
Vitalii Kotenko <caunt.official@gmail.com>
2025-04-10 18:05:11 +0300

    refactor(NbtByte): remove implicit conversion from NbtBoolean
    The implicit conversion operator from `NbtBoolean` to `NbtByte` has been removed.
    This change may impact interactions between these types in the codebase, particularly in cases where a `NbtBoolean` is expected to be treated as a `NbtByte`.

--- src/Minecraft/Nbt/Tags/NbtByte.cs
+++ src/Minecraft/Nbt/Tags/NbtByte.cs
@@ -9,7 +9,6 @@ public record NbtByte(byte Value = 0) : NbtTag
     public static implicit operator ByteTag(NbtByte tag) => new(tag.Name, tag.Value);
 
     public static implicit operator NbtBoolean(NbtByte tag) => new(Convert.ToBoolean(tag.Value)) { Name = tag.Name };
-    public static implicit operator NbtByte(NbtBoolean tag) => (ByteTag)tag;
 
     public override string ToString() => ToSnbt();
 }
