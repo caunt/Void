978e99f047567905d74b49fa86f4d74d393ac4ba
Vitalii Kotenko <caunt.official@gmail.com>
2025-04-11 18:40:44 +0300

    feat(config): add plugin support to IConfigurationService
    Added a new using directive for `Void.Common.Plugins` to introduce plugin-related functionality.
    Modified the `IConfigurationService` interface to include a new method `Get<TConfiguration>(IPlugin plugin)`, allowing retrieval of configurations associated with specific plugins, thereby enhancing the interface's capabilities.

--- src/Api/Configurations/IConfigurationService.cs
+++ src/Api/Configurations/IConfigurationService.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.Extensions.Hosting;
+using Void.Common.Plugins;
 
 namespace Void.Proxy.Api.Configurations;
 
-public interface IConfigurationService : IHostedService;
+public interface IConfigurationService : IHostedService
+{
+    public TConfiguration Get<TConfiguration>(IPlugin plugin) where TConfiguration : IConfiguration;
+}
