2ecac7774127fba2e0fb72e390ea711d7c4ed737
Vitalii Kotenko <caunt.official@gmail.com>
2025-04-11 03:34:31 +0300

    feat(buffers): enhance reading and writing methods
    Updated `ReadMinecraftBufferExtensions.cs` and `WriteMinecraftBufferExtensions.cs` to include new methods for reading and writing various data types from and to buffers, such as `ReadUnsignedByte`, `ReadBoolean`, `WriteVarInt`, and more. Renamed `ReadByte` and `WriteByte` to `ReadUnsignedByte` and `WriteUnsignedByte` for clarity. Introduced private core methods for handling variable-length data types to promote code reuse. Integrated `Uuid` and `Property` types for better handling of complex data structures, improving overall extensibility and usability in the Minecraft context.

--- src/Minecraft/Buffers/Extensions/ReadMinecraftBufferExtensions.cs
+++ src/Minecraft/Buffers/Extensions/ReadMinecraftBufferExtensions.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 using System.Buffers.Binary;
+using System.Text;
+using Void.Minecraft.Components.Text;
+using Void.Minecraft.Nbt;
+using Void.Minecraft.Network;
+using Void.Minecraft.Profiles;
 
 namespace Void.Minecraft.Buffers.Extensions;
 
@@ -11,10 +16,32 @@ public static class ReadMinecraftBufferExtensions
     /// <typeparam name="TBuffer">The type parameter represents a structure that implements a specific buffer interface.</typeparam>
     /// <param name="buffer">The parameter is a reference to a buffer from which a byte will be read.</param>
     /// <returns>Returns the byte read from the buffer.</returns>
-    public static byte ReadByte<TBuffer>(ref this TBuffer buffer)
+    public static byte ReadUnsignedByte<TBuffer>(ref this TBuffer buffer)
         where TBuffer : struct, IMinecraftBuffer<TBuffer>,
         allows ref struct =>
         buffer.Read(1)[0];
+
+    /// <summary>
+    /// Reads a boolean value from the provided buffer.
+    /// </summary>
+    /// <typeparam name="TBuffer">This type parameter represents a structure that implements a specific buffer interface for reading data.</typeparam>
+    /// <param name="buffer">This parameter is a reference to the buffer from which the boolean value is read.</param>
+    /// <returns>Returns the boolean value read from the buffer.</returns>
+    public static bool ReadBoolean<TBuffer>(ref this TBuffer buffer)
+        where TBuffer : struct, IMinecraftBuffer<TBuffer>,
+        allows ref struct =>
+        Convert.ToBoolean(buffer.ReadUnsignedByte());
+
+    /// <summary>
+    /// Reads a variable-length short value from a buffer.
+    /// </summary>
+    /// <typeparam name="TBuffer">This type parameter represents a structure that implements a specific buffer interface for reading data.</typeparam>
+    /// <param name="buffer">The reference to the buffer from which the variable-length short value is read.</param>
+    /// <returns>Returns the short value read from the buffer.</returns>
+    public static int ReadVarShort<TBuffer>(ref this TBuffer buffer)
+        where TBuffer : struct, IMinecraftBuffer<TBuffer>,
+        allows ref struct =>
+        ReadVarShortCore(ref buffer);
 
     /// <summary>
     /// Reads a 16-bit unsigned integer from a buffer in big-endian format.
@@ -37,6 +64,17 @@ public static class ReadMinecraftBufferExtensions
         where TBuffer : struct, IMinecraftBuffer<TBuffer>,
         allows ref struct =>
         BinaryPrimitives.ReadInt16BigEndian(buffer.Read(2));
+
+    /// <summary>
+    /// Reads a variable-length integer from a buffer.
+    /// </summary>
+    /// <typeparam name="TBuffer">This type parameter represents a structure that implements a specific buffer interface for reading data.</typeparam>
+    /// <param name="buffer">The parameter is a reference to the buffer from which the variable-length integer is read.</param>
+    /// <returns>Returns the integer value read from the buffer.</returns>
+    public static int ReadVarInt<TBuffer>(ref this TBuffer buffer)
+        where TBuffer : struct, IMinecraftBuffer<TBuffer>,
+        allows ref struct =>
+        ReadVarIntCore(ref buffer);
 
     /// <summary>
     /// Reads a 32-bit integer from a buffer in big-endian format.
@@ -72,6 +110,17 @@ public static class ReadMinecraftBufferExtensions
         BinaryPrimitives.ReadDoubleBigEndian(buffer.Read(8));
 
     /// <summary>
+    /// Reads a variable-length long value from a buffer.
+    /// </summary>
+    /// <typeparam name="TBuffer">This type parameter represents a structure that implements a specific buffer interface for reading data.</typeparam>
+    /// <param name="buffer">The parameter is a reference to the buffer from which the long value is read.</param>
+    /// <returns>Returns the long value read from the buffer.</returns>
+    public static long ReadVarLong<TBuffer>(ref this TBuffer buffer)
+        where TBuffer : struct, IMinecraftBuffer<TBuffer>,
+        allows ref struct =>
+        ReadVarLongCore(ref buffer);
+
+    /// <summary>
     /// Reads a 64-bit integer from a buffer in big-endian format.
     /// </summary>
     /// <typeparam name="TBuffer">The type parameter represents a structure that implements a specific buffer interface.</typeparam>
@@ -81,6 +130,92 @@ public static class ReadMinecraftBufferExtensions
         where TBuffer : struct, IMinecraftBuffer<TBuffer>,
         allows ref struct =>
         BinaryPrimitives.ReadInt64BigEndian(buffer.Read(8));
+
+    /// <summary>
+    /// Reads a UUID from a buffer by extracting two long values.
+    /// </summary>
+    /// <typeparam name="TBuffer">This type parameter represents a structure that implements a specific buffer interface for reading data.</typeparam>
+    /// <param name="buffer">This parameter is a reference to the buffer from which the UUID is read.</param>
+    /// <returns>Returns a UUID constructed from the two long values read from the buffer.</returns>
+    public static Uuid ReadUuid<TBuffer>(ref this TBuffer buffer)
+        where TBuffer : struct, IMinecraftBuffer<TBuffer>,
+        allows ref struct =>
+        Uuid.FromLongs(buffer.ReadLong(), buffer.ReadLong());
+
+    /// <summary>
+    /// Reads a UUID from a buffer and returns it as an integer array.
+    /// </summary>
+    /// <typeparam name="TBuffer">This type is a struct that implements a specific buffer interface for reading data.</typeparam>
+    /// <param name="buffer">The buffer is used to read the UUID data from its current position.</param>
+    /// <returns>An integer array representing the UUID.</returns>
+    public static Uuid ReadUuidAsIntArray<TBuffer>(ref this TBuffer buffer)
+        where TBuffer : struct, IMinecraftBuffer<TBuffer>,
+        allows ref struct =>
+        ReadUuidAsIntArrayCore(ref buffer);
+
+    /// <summary>
+    /// Reads a string from a buffer with a specified maximum length.
+    /// </summary>
+    /// <typeparam name="TBuffer">This type parameter represents a structure that implements a specific buffer interface.</typeparam>
+    /// <param name="buffer">This parameter is a reference to the buffer from which the string is read.</param>
+    /// <param name="maxLength">This parameter defines the maximum number of characters to read from the buffer.</param>
+    /// <returns>The method returns the string read from the buffer.</returns>
+    public static string ReadString<TBuffer>(ref this TBuffer buffer, int maxLength = 32767)
+        where TBuffer : struct, IMinecraftBuffer<TBuffer>,
+        allows ref struct =>
+        ReadStringCore(ref buffer, maxLength);
+
+    /// <summary>
+    /// Reads a property from a buffer and returns it.
+    /// </summary>
+    /// <typeparam name="TBuffer">The type parameter represents a structure that implements a specific buffer interface.</typeparam>
+    /// <param name="buffer">The parameter is a reference to the buffer from which the property is read.</param>
+    /// <returns>The method returns the property read from the buffer.</returns>
+    public static Property ReadProperty<TBuffer>(ref this TBuffer buffer)
+        where TBuffer : struct, IMinecraftBuffer<TBuffer>,
+        allows ref struct =>
+        ReadPropertyCore(ref buffer);
+
+    /// <summary>
+    /// Reads an array of properties from a buffer, allowing for a specified number of properties to be read.
+    /// </summary>
+    /// <typeparam name="TBuffer">This type parameter represents a structure that implements a specific buffer interface for reading data.</typeparam>
+    /// <param name="buffer">The buffer from which properties are read, passed by reference to allow modifications.</param>
+    /// <param name="count">Specifies the number of properties to read from the buffer, with a default value indicating all properties
+    /// should be read.</param>
+    /// <returns>An array of properties read from the buffer.</returns>
+    public static Property[] ReadPropertyArray<TBuffer>(ref this TBuffer buffer, int count = -1)
+        where TBuffer : struct, IMinecraftBuffer<TBuffer>,
+        allows ref struct =>
+        ReadPropertyArrayCore(ref buffer, count);
+
+    /// <summary>
+    /// Reads a tag from a buffer and returns it as an NbtTag object.
+    /// </summary>
+    /// <typeparam name="TBuffer">This type parameter represents a structure that implements a specific buffer interface for reading data.</typeparam>
+    /// <param name="buffer">This parameter is a reference to the buffer from which the tag is read.</param>
+    /// <returns>Returns an NbtTag object that represents the read tag.</returns>
+    public static NbtTag ReadTag<TBuffer>(ref this TBuffer buffer, bool readName = false)
+        where TBuffer : struct, IMinecraftBuffer<TBuffer>,
+        allows ref struct =>
+        NbtTag.ReadFrom(ref buffer, readName);
+
+    /// <summary>
+    /// Reads a component from a specified buffer using a given protocol version.
+    /// </summary>
+    /// <typeparam name="TBuffer">This type parameter represents a structure that implements a specific buffer interface for reading data.</typeparam>
+    /// <param name="buffer">This parameter is the data source from which the component is read.</param>
+    /// <param name="protocolVersion">This parameter indicates the version of the protocol to be used for reading the component.</param>
+    /// <returns>Returns the component that has been read from the buffer.</returns>
+    public static Component ReadComponent<TBuffer>(ref this TBuffer buffer, ProtocolVersion protocolVersion)
+        where TBuffer : struct, IMinecraftBuffer<TBuffer>,
+        allows ref struct =>
+        Component.ReadFrom(ref buffer, protocolVersion);
+
+    public static ReadOnlySpan<byte> ReadToEnd<TBuffer>(ref this TBuffer buffer)
+        where TBuffer : struct, IMinecraftBuffer<TBuffer>,
+        allows ref struct =>
+        buffer.Read(buffer.Remaining);
 
     /// <summary>
     /// Reads a specified number of bytes from a buffer and returns them as a read-only span.
@@ -94,5 +229,114 @@ public static class ReadMinecraftBufferExtensions
         var slice = buffer.Access(length);
         buffer.Seek(length);
         return slice;
+    }
+
+    private static int ReadVarShortCore<TBuffer>(ref TBuffer buffer) where TBuffer : struct, IMinecraftBuffer<TBuffer>, allows ref struct
+    {
+        var low = buffer.ReadUnsignedShort();
+        var high = 0;
+
+        if ((low & 0x8000) == 0)
+            return (high & 0xFF) << 15 | low;
+
+        low &= 0x7FFF;
+        high = buffer.ReadUnsignedByte();
+
+        return (high & 0xFF) << 15 | low;
+    }
+
+    private static int ReadVarIntCore<TBuffer>(ref TBuffer buffer) where TBuffer : struct, IMinecraftBuffer<TBuffer>, allows ref struct
+    {
+        var result = 0;
+
+        byte read = 0;
+        byte temp;
+
+        do
+        {
+            temp = buffer.ReadUnsignedByte();
+            var value = temp & 0b01111111;
+
+            result |= value << 7 * read;
+            read++;
+
+            if (read > 5)
+                throw new InvalidOperationException("VarInt is too big");
+        }
+        while ((temp & 0b10000000) != 0);
+
+        return result;
+    }
+
+    private static long ReadVarLongCore<TBuffer>(ref TBuffer buffer) where TBuffer : struct, IMinecraftBuffer<TBuffer>, allows ref struct
+    {
+        var result = 0;
+
+        byte read = 0;
+        byte temp;
+
+        do
+        {
+            temp = buffer.ReadUnsignedByte();
+            var value = temp & 0b01111111;
+
+            result |= value << 7 * read;
+            read++;
+
+            if (read > 10)
+                throw new InvalidOperationException("VarLong is too big");
+        }
+        while ((temp & 0b10000000) != 0);
+
+        return result;
+    }
+
+    private static Uuid ReadUuidAsIntArrayCore<TBuffer>(ref TBuffer buffer) where TBuffer : struct, IMinecraftBuffer<TBuffer>, allows ref struct
+    {
+        var msbHigh = (long)buffer.ReadInt() << 32;
+        var msbLow = buffer.ReadInt() & 0xFFFFFFFFL;
+        var msb = msbHigh | msbLow;
+
+        var lsbHigh = (long)buffer.ReadInt() << 32;
+        var lsbLow = buffer.ReadInt() & 0xFFFFFFFFL;
+        var lsb = lsbHigh | lsbLow;
+
+        return Uuid.FromLongs(msb, lsb);
+    }
+
+    private static string ReadStringCore<TBuffer>(ref TBuffer buffer, int maxLength = 32767) where TBuffer : struct, IMinecraftBuffer<TBuffer>, allows ref struct
+    {
+        var length = buffer.ReadVarInt();
+        var span = buffer.Read(length);
+
+        var value = Encoding.UTF8.GetString(span);
+
+        if (maxLength > 0 && value.Length > maxLength)
+            throw new IndexOutOfRangeException($"string ({value.Length}) exceeded maximum length ({maxLength})");
+
+        return value;
+    }
+
+    private static Property ReadPropertyCore<TBuffer>(ref TBuffer buffer) where TBuffer : struct, IMinecraftBuffer<TBuffer>, allows ref struct
+    {
+        var name = buffer.ReadString();
+        var value = buffer.ReadString();
+        var isSigned = buffer.ReadBoolean();
+        var signature = isSigned ? buffer.ReadString() : null;
+
+        return new Property(name, value, isSigned, signature);
+    }
+
+    private static Property[] ReadPropertyArrayCore<TBuffer>(ref TBuffer buffer, int count = -1) where TBuffer : struct, IMinecraftBuffer<TBuffer>, allows ref struct
+    {
+        if (count < 0)
+            count = buffer.ReadVarInt();
+
+        var properties = new Property[count];
+
+        for (var i = 0; i < count; i++)
+            properties[i] = buffer.ReadProperty();
+
+        return properties;
     }
 }
--- src/Minecraft/Buffers/Extensions/WriteMinecraftBufferExtensions.cs
+++ src/Minecraft/Buffers/Extensions/WriteMinecraftBufferExtensions.cs
@@ -1,5 +1,11 @@
 ﻿using System;
 using System.Buffers.Binary;
+using System.IO;
+using System.Text;
+using Void.Minecraft.Components.Text;
+using Void.Minecraft.Nbt;
+using Void.Minecraft.Network;
+using Void.Minecraft.Profiles;
 
 namespace Void.Minecraft.Buffers.Extensions;
 
@@ -11,10 +17,32 @@ public static class WriteMinecraftBufferExtensions
     /// <typeparam name="TBuffer">This type parameter represents a structure that implements a specific buffer interface.</typeparam>
     /// <param name="buffer">The buffer is modified to store the byte value at a designated position.</param>
     /// <param name="value">The byte value to be written into the buffer.</param>
-    public static void WriteByte<TBuffer>(ref this TBuffer buffer, byte value)
+    public static void WriteUnsignedByte<TBuffer>(ref this TBuffer buffer, byte value)
         where TBuffer : struct, IMinecraftBuffer<TBuffer>,
         allows ref struct =>
         buffer.AccessWrite(1)[0] = value;
+
+    /// <summary>
+    /// Writes a boolean value to a specified buffer by converting it to a byte.
+    /// </summary>
+    /// <typeparam name="TBuffer">This type parameter represents a structure that implements a specific buffer interface for writing data.</typeparam>
+    /// <param name="buffer">The buffer is the destination where the boolean value will be written.</param>
+    /// <param name="value">The boolean value to be converted to a byte and written to the buffer.</param>
+    public static void WriteBoolean<TBuffer>(ref this TBuffer buffer, bool value)
+        where TBuffer : struct, IMinecraftBuffer<TBuffer>,
+        allows ref struct =>
+        buffer.WriteUnsignedByte(Convert.ToByte(value));
+
+    /// <summary>
+    /// Writes a variable-length short integer to the specified buffer.
+    /// </summary>
+    /// <typeparam name="TBuffer">This type is a struct that implements a specific buffer interface for Minecraft data handling.</typeparam>
+    /// <param name="buffer">The buffer is the destination where the variable-length short integer will be written.</param>
+    /// <param name="value">The integer value to be written to the buffer in a variable-length format.</param>
+    public static void WriteVarShort<TBuffer>(ref this TBuffer buffer, int value)
+        where TBuffer : struct, IMinecraftBuffer<TBuffer>,
+        allows ref struct =>
+        WriteVarShortCore(ref buffer, value);
 
     /// <summary>
     /// Writes an unsigned short value to a specified buffer in big-endian format.
@@ -38,6 +66,11 @@ public static class WriteMinecraftBufferExtensions
         allows ref struct =>
         BinaryPrimitives.WriteInt16BigEndian(buffer.AccessWrite(2), value);
 
+    public static void WriteVarInt<TBuffer>(ref this TBuffer buffer, int value)
+        where TBuffer : struct, IMinecraftBuffer<TBuffer>,
+        allows ref struct =>
+        WriteVarIntCore(ref buffer, value);
+
     /// <summary>
     /// Writes a 32-bit integer to a specified buffer in big-endian format.
     /// </summary>
@@ -48,7 +81,6 @@ public static class WriteMinecraftBufferExtensions
         where TBuffer : struct, IMinecraftBuffer<TBuffer>,
         allows ref struct =>
         BinaryPrimitives.WriteInt32BigEndian(buffer.AccessWrite(4), value);
-
 
     /// <summary>
     /// Writes a floating-point number in big-endian format to a specified buffer.
@@ -61,7 +93,6 @@ public static class WriteMinecraftBufferExtensions
         allows ref struct =>
         BinaryPrimitives.WriteSingleBigEndian(buffer.AccessWrite(4), value);
 
-
     /// <summary>
     /// Writes a double value to a specified buffer in big-endian format.
     /// </summary>
@@ -73,6 +104,16 @@ public static class WriteMinecraftBufferExtensions
         allows ref struct =>
         BinaryPrimitives.WriteDoubleBigEndian(buffer.AccessWrite(8), value);
 
+    /// <summary>
+    /// Writes a variable-length long integer to a buffer.
+    /// </summary>
+    /// <typeparam name="TBuffer">This type is a struct that implements a specific buffer interface for Minecraft data handling.</typeparam>
+    /// <param name="buffer">The buffer to which the variable-length long integer will be written.</param>
+    /// <param name="value">The long integer value that will be written to the buffer.</param>
+    public static void WriteVarLong<TBuffer>(ref this TBuffer buffer, long value)
+        where TBuffer : struct, IMinecraftBuffer<TBuffer>,
+        allows ref struct =>
+        WriteVarLongCore(ref buffer, value);
 
     /// <summary>
     /// Writes a 64-bit integer to a specified buffer in big-endian format.
@@ -86,6 +127,88 @@ public static class WriteMinecraftBufferExtensions
         BinaryPrimitives.WriteInt64BigEndian(buffer.AccessWrite(8), value);
 
     /// <summary>
+    /// Writes a UUID value to a specified buffer in a Minecraft-compatible format.
+    /// </summary>
+    /// <typeparam name="TBuffer">This type parameter represents a structure that implements a specific buffer interface for Minecraft data
+    /// handling.</typeparam>
+    /// <param name="buffer">The buffer is the destination where the UUID will be written.</param>
+    /// <param name="value">The UUID value to be converted and written to the buffer.</param>
+    public static void WriteUuid<TBuffer>(ref this TBuffer buffer, Uuid value)
+        where TBuffer : struct, IMinecraftBuffer<TBuffer>,
+        allows ref struct =>
+        buffer.Write(value.AsGuid.ToByteArray(true));
+
+    /// <summary>
+    /// Writes a UUID as an integer array into a specified buffer.
+    /// </summary>
+    /// <typeparam name="TBuffer">This type parameter represents a structure that implements a specific buffer interface for Minecraft.</typeparam>
+    /// <param name="buffer">The buffer is modified to include the integer representation of the UUID.</param>
+    /// <param name="value">The UUID is converted and written into the buffer as an array of integers.</param>
+    public static void WriteUuidAsIntArray<TBuffer>(ref this TBuffer buffer, Uuid value)
+        where TBuffer : struct, IMinecraftBuffer<TBuffer>,
+        allows ref struct =>
+        WriteUuidAsIntArrayCore(ref buffer, value);
+
+    /// <summary>
+    /// Writes a string to a specified buffer.
+    /// </summary>
+    /// <typeparam name="TBuffer">This type parameter represents a structure that implements a specific buffer interface for Minecraft.</typeparam>
+    /// <param name="buffer">This parameter is a reference to the buffer where the string will be written.</param>
+    /// <param name="value">This parameter is the string that will be written to the buffer.</param>
+    public static void WriteString<TBuffer>(ref this TBuffer buffer, string value)
+        where TBuffer : struct, IMinecraftBuffer<TBuffer>,
+        allows ref struct =>
+        WriteStringCore(ref buffer, value);
+
+    /// <summary>
+    /// Writes a property to a specified buffer.
+    /// </summary>
+    /// <typeparam name="TBuffer">This type parameter represents a structure that implements a specific buffer interface for Minecraft data
+    /// handling.</typeparam>
+    /// <param name="buffer">This parameter is a reference to the buffer where the property will be written.</param>
+    /// <param name="value">This parameter represents the property that is to be written to the buffer.</param>
+    public static void WriteProperty<TBuffer>(ref this TBuffer buffer, Property value)
+        where TBuffer : struct, IMinecraftBuffer<TBuffer>,
+        allows ref struct =>
+        WritePropertyCore(ref buffer, value);
+
+    /// <summary>
+    /// Writes an array of properties to a specified buffer.
+    /// </summary>
+    /// <typeparam name="TBuffer">This type is a structure that implements a specific buffer interface for Minecraft data handling.</typeparam>
+    /// <param name="buffer">The buffer to which the array of properties will be written.</param>
+    /// <param name="value">The array of properties that will be written to the buffer.</param>
+    public static void WritePropertyArray<TBuffer>(ref this TBuffer buffer, Property[] value)
+        where TBuffer : struct, IMinecraftBuffer<TBuffer>,
+        allows ref struct =>
+        WritePropertyArrayCore(ref buffer, value);
+
+    /// <summary>
+    /// Writes a tag to a specified buffer using a stream representation of the tag.
+    /// </summary>
+    /// <typeparam name="TBuffer">This type parameter represents a structure that implements a specific buffer interface for Minecraft data
+    /// handling.</typeparam>
+    /// <param name="buffer">This parameter is a reference to the buffer where the tag will be written.</param>
+    /// <param name="value">This parameter represents the tag that will be converted to a stream and written to the buffer.</param>
+    public static void WriteTag<TBuffer>(ref this TBuffer buffer, NbtTag value)
+        where TBuffer : struct, IMinecraftBuffer<TBuffer>,
+        allows ref struct =>
+        buffer.Write(value.AsStream());
+
+    /// <summary>
+    /// Writes a component to a specified buffer using a given protocol version.
+    /// </summary>
+    /// <typeparam name="TBuffer">This type parameter represents a structure that implements a specific buffer interface for Minecraft data
+    /// handling.</typeparam>
+    /// <param name="buffer">This parameter is a reference to the buffer where the component will be written.</param>
+    /// <param name="value">This parameter represents the component that will be written to the buffer.</param>
+    /// <param name="protocolVersion">This parameter indicates the version of the protocol to be used during the write operation.</param>
+    public static void WriteComponent<TBuffer>(ref this TBuffer buffer, Component value, ProtocolVersion protocolVersion)
+        where TBuffer : struct, IMinecraftBuffer<TBuffer>,
+        allows ref struct =>
+        value.WriteTo(ref buffer, protocolVersion);
+
+    /// <summary>
     /// Writes a byte span to a buffer, allowing for efficient data handling in a structured format.
     /// </summary>
     /// <typeparam name="TBuffer">This type parameter represents a struct that implements a specific buffer interface for Minecraft data
@@ -97,10 +220,96 @@ public static class WriteMinecraftBufferExtensions
         allows ref struct =>
         data.CopyTo(buffer.AccessWrite(data.Length));
 
+    /// <summary>
+    /// Writes data from a stream to a specified buffer. Requires the stream to support length property.
+    /// </summary>
+    /// <typeparam name="TBuffer">The type parameter represents a structure that can be used as a buffer for writing data.</typeparam>
+    /// <param name="buffer">The buffer is a reference to the structure that will receive the data from the stream.</param>
+    /// <param name="data">The stream provides the data source that will be written into the buffer.</param>
+    public static void Write<TBuffer>(ref this TBuffer buffer, Stream value)
+        where TBuffer : struct, IMinecraftBuffer<TBuffer>,
+        allows ref struct =>
+        value.ReadExactly(buffer.AccessWrite((int)value.Length));
+
     private static Span<byte> AccessWrite<TBuffer>(ref this TBuffer buffer, int length) where TBuffer : struct, IMinecraftBuffer<TBuffer>, allows ref struct
     {
         var span = buffer.Access(length);
         buffer.Seek(length);
         return span;
+    }
+
+    private static void WriteVarShortCore<TBuffer>(ref TBuffer buffer, int value) where TBuffer : struct, IMinecraftBuffer<TBuffer>, allows ref struct
+    {
+        var low = (ushort)(value & 0x7FFF);
+        var high = (byte)((value & 0x7F8000) >> 15);
+
+        if (high != 0)
+            low |= 0x8000;
+
+        buffer.WriteUnsignedShort(low);
+
+        if (high != 0)
+            buffer.WriteUnsignedByte(high);
+    }
+
+    private static void WriteVarIntCore<TBuffer>(ref TBuffer buffer, int value) where TBuffer : struct, IMinecraftBuffer<TBuffer>, allows ref struct
+    {
+        Span<byte> valueBuffer = stackalloc byte[5];
+        var length = value.AsVarInt(valueBuffer);
+        buffer.Write(valueBuffer[..length]);
+    }
+
+    private static void WriteVarLongCore<TBuffer>(ref TBuffer buffer, long value) where TBuffer : struct, IMinecraftBuffer<TBuffer>, allows ref struct
+    {
+        while (true)
+        {
+            if ((value & ~0x7F) == 0)
+            {
+                buffer.WriteUnsignedByte((byte)value);
+                return;
+            }
+
+            buffer.WriteUnsignedByte((byte)((value & 0x7F) | 0x80));
+            value >>>= 7;
+        }
+    }
+
+    private static void WriteUuidAsIntArrayCore<TBuffer>(ref TBuffer buffer, Uuid value) where TBuffer : struct, IMinecraftBuffer<TBuffer>, allows ref struct
+    {
+        var span = value.AsGuid.ToByteArray().AsSpan();
+
+        buffer.WriteInt(BitConverter.ToInt32(span[..4]));
+        buffer.WriteInt(BitConverter.ToInt32(span[4..8]));
+        buffer.WriteInt(BitConverter.ToInt32(span[8..12]));
+        buffer.WriteInt(BitConverter.ToInt32(span[12..16]));
+    }
+
+    private static void WriteStringCore<TBuffer>(ref TBuffer buffer, string value) where TBuffer : struct, IMinecraftBuffer<TBuffer>, allows ref struct
+    {
+        var bytes = Encoding.UTF8.GetBytes(value);
+        bytes.CopyTo(buffer.AccessWrite(bytes.Length));
+    }
+
+    private static void WritePropertyCore<TBuffer>(ref TBuffer buffer, Property value) where TBuffer : struct, IMinecraftBuffer<TBuffer>, allows ref struct
+    {
+        buffer.WriteString(value.Name);
+        buffer.WriteString(value.Value);
+        buffer.WriteBoolean(value.IsSigned);
+
+        if (!value.IsSigned)
+            return;
+
+        if (string.IsNullOrWhiteSpace(value.Signature))
+            throw new InvalidDataException("Signature is null or whitespace, but IsSigned set to true");
+
+        buffer.WriteString(value.Signature);
+    }
+
+    private static void WritePropertyArrayCore<TBuffer>(ref TBuffer buffer, Property[] value) where TBuffer : struct, IMinecraftBuffer<TBuffer>, allows ref struct
+    {
+        buffer.WriteVarInt(value.Length);
+
+        foreach (var property in value)
+            buffer.WriteProperty(property);
     }
 }
