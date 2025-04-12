b98515d807288c5af04251b26404c9f2f2b38a31
Vitalii Kotenko <caunt.official@gmail.com>
2025-04-12 19:17:52 +0300

    feat(MessageReceivedEvent): add cancellation support
    Added a new property `Result` to the `MessageReceivedEvent` class to indicate if a packet should not be sent. Introduced a `Cancel()` method that allows for canceling an operation, setting `Result` to `true` upon cancellation, and returning whether the operation was already canceled.

--- src/Api/Events/Network/MessageReceivedEvent.cs
+++ src/Api/Events/Network/MessageReceivedEvent.cs
@@ -11,4 +11,17 @@ public record MessageReceivedEvent(Side Origin, Side From, Side To, Direction Di
     ///     <see langword="true" /> if packet should not be sent
     /// </summary>
     public bool Result { get; set; }
+
+    /// <summary>
+    /// Cancels an operation if it hasn't been canceled already. It sets the Result to true upon cancellation.
+    /// </summary>
+    /// <returns>Returns true if the operation was already canceled; otherwise, returns false.</returns>
+    public bool Cancel()
+    {
+        if (Result)
+            return true;
+
+        Result = true;
+        return false;
+    }
 }
