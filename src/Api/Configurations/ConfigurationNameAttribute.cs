f4557710e9839692c1e4a415d5508d329809ad8c
Vitalii Kotenko <caunt.official@gmail.com>
2025-04-12 15:21:32 +0300

    feat(configurations): add ConfigurationNameAttribute class
    A new class named `ConfigurationNameAttribute` has been added to the `Void.Proxy.Api.Configurations` namespace. This class is defined as an attribute that can be applied to interfaces, classes, and structs. It takes a string parameter `name` in its constructor and exposes it as a read-only property `Name`.

---
+++ src/Api/Configurations/ConfigurationNameAttribute.cs
@@ -0,0 +1,7 @@
+﻿namespace Void.Proxy.Api.Configurations;
+
+[AttributeUsage(AttributeTargets.Interface | AttributeTargets.Class | AttributeTargets.Struct)]
+public class ConfigurationNameAttribute(string name) : Attribute
+{
+    public string Name { get; } = name;
+}
