e74d5ccfee5c62e9b0c945ef8696121d52190101
Vitalii Kotenko <caunt.official@gmail.com>
2025-04-14 00:10:58 +0300

    feat(serializer): add methods for object serialization
    Added two new methods to the `IConfigurationSerializer` interface:
    `Serialize(object configuration)` and `Serialize(object? configuration, Type configurationType)`.
    These methods enhance the flexibility of the interface by allowing for serialization of both
    non-nullable and nullable object configurations, accommodating a wider range of configuration types.

--- src/Api/Configurations/Serializer/IConfigurationSerializer.cs
+++ src/Api/Configurations/Serializer/IConfigurationSerializer.cs
@@ -4,6 +4,7 @@ public interface IConfigurationSerializer
 {
     public string Serialize<TConfiguration>() where TConfiguration : notnull;
     public string Serialize(Type configurationType);
+    public string Serialize(object configuration);
     public string Serialize<TConfiguration>(TConfiguration? configuration) where TConfiguration : notnull;
     public string Serialize(object? configuration, Type configurationType);
     public TConfiguration Deserialize<TConfiguration>(string source) where TConfiguration : notnull;
