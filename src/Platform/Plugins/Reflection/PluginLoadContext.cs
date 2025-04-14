866ad7c28155c849e61850589858008b4031bdc3
Vitalii Kotenko <gpg@caunt.world>
2025-04-14 22:02:24 +0300

    test GPG commit

--- src/Platform/Plugins/Reflection/PluginLoadContext.cs
+++ src/Platform/Plugins/Reflection/PluginLoadContext.cs
@@ -25,6 +25,8 @@ public class PluginLoadContext : AssemblyLoadContext
             _localDependencies = new AssemblyDependencyResolver(componentAssemblyPath);
 
         PluginAssembly = LoadFromStream(assemblyStream);
+
+        // test GPG commit
     }
 
     public Assembly PluginAssembly { get; }
