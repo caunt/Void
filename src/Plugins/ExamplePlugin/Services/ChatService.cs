64aff839959a80ddc494084a9ad111fbc081c93d
Vitalii Kotenko <gpg@caunt.world>
2025-04-15 00:41:29 +0300

    feat(chat): enhance chat service with settings loading
    - Added a new using directive for `Void.Proxy.Api.Events.Proxy`.
    - Introduced a private `_settings` field in `ChatService` to store chat settings.
    - Implemented `OnProxyStarting` method to load settings asynchronously on proxy start.
    - Modified `OnPhaseChanged` to utilize the loaded `_settings` and added null checks.
    - Updated `WelcomeMessage` property in `ChatSettings` to use string interpolation for better readability.

--- src/Plugins/ExamplePlugin/Services/ChatService.cs
+++ src/Plugins/ExamplePlugin/Services/ChatService.cs
@@ -5,23 +5,41 @@ using Void.Minecraft.Network;
 using Void.Minecraft.Players.Extensions;
 using Void.Proxy.Api.Configurations;
 using Void.Proxy.Api.Events;
+using Void.Proxy.Api.Events.Proxy;
 using Void.Proxy.Api.Links;
 
 namespace Void.Proxy.Plugins.ExamplePlugin.Services;
 
 public class ChatService(ILinkService links, IConfigurationService configs) : IEventListener
 {
+    private ChatSettings? _settings;
+
+    [Subscribe]
+    public async ValueTask OnProxyStarting(ProxyStartingEvent @event, CancellationToken cancellationToken)
+    {
+        // Configurations can be loaded once, and any changes made to them will be automatically saved to disk. 
+        // Vice versa, any changes made to the configuration file on disk will be automatically loaded into the instance.
+        // You do not need to worry about saving or loading the configuration manually.
+        _settings = await configs.GetAsync<ChatSettings>(cancellationToken);
+    }
+
     [Subscribe]
     public async ValueTask OnPhaseChanged(PhaseChangedEvent @event, CancellationToken cancellationToken)
     {
+        // Send message only when the connection is in the Play phase on the client side
         if (@event is not { Phase: Phase.Play, Side: Side.Client })
             return;
 
+        // Only if player has active link to the server
         if (!links.TryGetLink(@event.Player, out var link))
             return;
 
-        var settings = await configs.GetAsync<ChatSettings>(cancellationToken);
-        var message = settings.WelcomeMessage.Replace("%SERVER%", link.Server.ToString());
+        // If settings are not loaded, do nothing. Practically impossible, but just in case.
+        if (_settings is null)
+            return;
+
+        // Replace placeholder in the message with the server name
+        var message = _settings.WelcomeMessage.Replace("%SERVER%", link.Server.Name);
 
         await @event.Player.SendChatMessageAsync(message, cancellationToken);
     }
@@ -29,5 +47,5 @@ public class ChatService(ILinkService links, IConfigurationService configs) : IE
 
 public class ChatSettings
 {
-    public string WelcomeMessage { get; set; } = "Welcome to the %SERVER% server!";
+    public string WelcomeMessage { get; set; } = $"Welcome to the %SERVER% server!";
 }
