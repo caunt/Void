cb4bdd3f9c8c5deea611f3b4b7ff6608ac93b44d
Vitalii Kotenko <gpg@caunt.world>
2025-04-16 20:56:43 +0300

    feat(dependency-service): enhance service management functionality
    Updated the IDependencyService interface to include new methods for service registration and retrieval. Added a private dictionary in DependencyService to manage plugin-associated service providers. Introduced the All property to access all registered services and implemented an event handler for plugin unloading. Added methods for retrieving services with configuration functions and for registering services through a ServiceCollection. A private method was also added to aggregate services from all plugins into a single ServiceProvider.

--- src/Api/Plugins/IDependencyService.cs
+++ src/Api/Plugins/IDependencyService.cs
@@ -1,4 +1,5 @@
 ﻿
+using Microsoft.Extensions.DependencyInjection;
 using Void.Common.Events;
 
 namespace Void.Proxy.Api.Plugins;
@@ -9,4 +10,6 @@ public interface IDependencyService : IEventListener
     public TService CreateInstance<TService>(Type serviceType);
     public object CreateInstance(Type serviceType);
     public TService GetRequiredService<TService>() where TService : notnull;
+    public TService Get<TService>(Func<IServiceProvider, TService> configure);
+    public void Register(Action<ServiceCollection> configure);
 }
--- src/Platform/Plugins/Dependencies/DependencyService.cs
+++ src/Platform/Plugins/Dependencies/DependencyService.cs
@@ -1,11 +1,29 @@
 ﻿using Void.Common.Events;
+using Void.Common.Plugins;
+using Void.Proxy.Api.Events;
+using Void.Proxy.Api.Events.Plugins;
 using Void.Proxy.Api.Events.Services;
+using Void.Proxy.Api.Extensions;
 using Void.Proxy.Api.Plugins;
 
 namespace Void.Proxy.Plugins.Dependencies;
 
 public class DependencyService(IServiceProvider services, IEventService events) : IDependencyService
 {
+    private readonly Dictionary<IPlugin, IServiceProvider> _pluginServices = new();
+
+    public IServiceProvider All => GetAll();
+
+    [Subscribe]
+    public void OnPluginUnload(PluginUnloadEvent @event)
+    {
+        if (!_pluginServices.TryGetValue(@event.Plugin, out var serviceProvider))
+            return;
+
+        serviceProvider.RemoveServicesByAssembly(@event.Plugin.GetType().Assembly);
+        _pluginServices.Remove(@event.Plugin);
+    }
+
     public TService CreateInstance<TService>()
     {
         return CreateInstance<TService>(typeof(TService));
@@ -29,5 +47,26 @@ public class DependencyService(IServiceProvider services, IEventService events) 
     public TService GetRequiredService<TService>() where TService : notnull
     {
         return services.GetRequiredService<TService>();
+    }
+
+    public TService Get<TService>(Func<IServiceProvider, TService> configure)
+    {
+        return configure(All);
+    }
+
+    public void Register(Action<ServiceCollection> configure)
+    {
+        var services = new ServiceCollection();
+        configure(services);
+    }
+
+    private ServiceProvider GetAll()
+    {
+        var forwardedServices = new ServiceCollection();
+
+        foreach (var services in _pluginServices.Values)
+            services.ForwardServices(forwardedServices);
+
+        return forwardedServices.BuildServiceProvider();
     }
 }
