40e049b5c0dd6e94cc46b9ce3ca3364647addc6e
Vitalii Kotenko <gpg@caunt.world>
2025-04-17 22:14:32 +0300

    feat: add command registration functionality
    - Introduced new `CommandServiceExtensions` class in `Void.Minecraft.Commands.Extensions` for registering command configurations.
    - Added a second `CommandServiceExtensions` class in `Void.Proxy.Commands.Extensions` with a commented-out method for default command registration.
    - Updated `Platform.cs` to include a using directive for `Void.Proxy.Commands.Extensions`, enabling access to the new command functionalities.

---
+++ src/Minecraft/Commands/Extensions/CommandServiceExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Void.Minecraft.Commands.Brigadier;
+using Void.Minecraft.Commands.Brigadier.Builder;
+using Void.Proxy.Api.Commands;
+
+namespace Void.Minecraft.Commands.Extensions;
+
+public static class CommandServiceExtensions
+{
+    public static void Register(this ICommandService commands, Func<IArgumentContext, LiteralArgumentBuilder> configure)
+    {
+        commands.Dispatcher.Add(configure(default(ArgumentContext)).Build());
+    }
+}
--- src/Platform/Platform.cs
+++ src/Platform/Platform.cs
@@ -12,6 +12,7 @@ using Void.Proxy.Api.Players.Extensions;
 using Void.Proxy.Api.Plugins;
 using Void.Proxy.Api.Servers;
 using Void.Proxy.Api.Settings;
+using Void.Proxy.Commands.Extensions;
 
 namespace Void.Proxy;
 
