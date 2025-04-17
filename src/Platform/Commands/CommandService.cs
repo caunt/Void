ba84d9b26b1a8e3f5e5033d4ab579ec32e4574ea
Vitalii Kotenko <gpg@caunt.world>
2025-04-18 02:36:25 +0300

    chore: update CommandService for internal handling
    - Streamlined `using` directives by removing unused imports.
    - Changed `CommandService` constructor to a default constructor, altering dependency management.
    - Introduced a private `_dispatcher` field for internal command dispatching.
    - Added `Dispatcher` property to expose the `_dispatcher`.
    - Removed private methods for server control and player management, indicating a shift in functionality.
    - Modified `CompleteAsync` to return suggestion texts, changing how suggestions are generated.

--- src/Platform/Commands/CommandService.cs
+++ src/Platform/Commands/CommandService.cs
@@ -1,15 +1,10 @@
-﻿using Nito.Disposables.Internals;
-using Void.Common.Commands;
+﻿using Void.Common.Commands;
 using Void.Minecraft.Commands.Brigadier;
-using Void.Minecraft.Commands.Brigadier.Context;
-using Void.Minecraft.Commands.Brigadier.Suggestion;
-using Void.Minecraft.Players.Extensions;
 using Void.Proxy.Api.Commands;
-using Void.Proxy.Api.Players;
 
 namespace Void.Proxy.Commands;
 
-public class CommandService(ILogger<CommandService> logger, IPlayerService players, IHostApplicationLifetime hostApplicationLifetime) : ICommandService
+public class CommandService : ICommandService
 {
     private readonly CommandDispatcher _dispatcher = new();
 
@@ -27,41 +22,5 @@ public class CommandService(ILogger<CommandService> logger, IPlayerService playe
     {
         var suggestions = await _dispatcher.SuggestAsync(input, source, cancellationToken);
         return [.. suggestions.All.Select(suggestion => suggestion.Text)];
-    }
-
-    private int StopServer(CommandContext context)
-    {
-        logger.LogInformation("Stopping proxy...");
-        hostApplicationLifetime.StopApplication();
-        return 1;
-    }
-
-    private async ValueTask<int> KickPlayerAsync(CommandContext context, CancellationToken cancellationToken)
-    {
-        if (players.TryGetByName(context.GetArgument<string>("username"), out var player))
-        {
-            if (!player.TryGetMinecraftPlayer(out var minecraftPlayer))
-                return 0;
-
-            await players.KickPlayerAsync(minecraftPlayer, "You have been kicked by an operator.", cancellationToken);
-            return 1;
-        }
-
-        return 0;
-    }
-
-    private Suggestions SuggestPlayer(CommandContext context, SuggestionsBuilder builder)
-    {
-        return Suggestions.Create(context.Input, players.All.Select(player =>
-        {
-            if (!player.TryGetMinecraftPlayer(out var minecraftPlayer))
-                return null;
-
-            if (minecraftPlayer.Profile is null)
-                return null;
-
-            var name = minecraftPlayer.Profile.Username;
-            return new Suggestion(StringRange.Between(0, context.Input.Length), name);
-        }).WhereNotNull());
     }
 }
