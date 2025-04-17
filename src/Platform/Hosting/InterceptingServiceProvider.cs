1beed480aaf315a89f3379f929b4f916a8626bc8
Vitalii Kotenko <gpg@caunt.world>
2025-04-17 05:36:06 +0300

    feat: add intercepting service provider classes
    Introduce `InterceptingServiceProvider` and `InterceptingServiceProviderFactory` classes in the `Void.Proxy.Hosting` namespace. The `InterceptingServiceProvider` wraps an existing `IServiceProvider`, registering event listeners for `IEventListener` services and implementing resource management through `Dispose` and `DisposeAsync`. The factory class facilitates the creation of the service provider and service collection.

---
+++ src/Platform/Hosting/InterceptingServiceProvider.cs
@@ -0,0 +1,49 @@
+﻿using Void.Common.Events;
+using Void.Proxy.Api.Events.Services;
+
+namespace Void.Proxy.Hosting;
+
+public class InterceptingServiceProvider(IServiceProvider provider) : IServiceProvider, ISupportRequiredService, IDisposable, IAsyncDisposable
+{
+    private readonly IEventService _events = provider.GetRequiredService<IEventService>();
+
+    public object? GetService(Type serviceType)
+    {
+        var instance = provider.GetService(serviceType);
+
+        if (instance is IEventListener listener)
+            _events.RegisterListeners(listener);
+
+        return instance;
+    }
+
+    public object GetRequiredService(Type serviceType)
+    {
+        var instance = ((ISupportRequiredService)provider).GetRequiredService(serviceType);
+
+        if (instance is IEventListener listener)
+            _events.RegisterListeners(listener);
+
+        return instance;
+    }
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+
+        if (provider is not IDisposable disposable)
+            return;
+
+        disposable.Dispose();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        GC.SuppressFinalize(this);
+
+        if (provider is IAsyncDisposable disposable)
+            await disposable.DisposeAsync();
+        else
+            Dispose();
+    }
+}
---
+++ src/Platform/Hosting/InterceptingServiceProviderFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace Void.Proxy.Hosting;
+
+public class InterceptingServiceProviderFactory : IServiceProviderFactory<IServiceCollection>
+{
+    public IServiceCollection CreateBuilder(IServiceCollection services) => services;
+
+    public IServiceProvider CreateServiceProvider(IServiceCollection services)
+    {
+        var provider = services.BuildServiceProvider();
+        return new InterceptingServiceProvider(provider);
+    }
+}
