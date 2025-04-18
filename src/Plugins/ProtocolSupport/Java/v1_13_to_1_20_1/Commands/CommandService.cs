0f9b7d759aee514a3ac94a4d8d1ba7ccf8c8c57d
Vitalii Kotenko <gpg@caunt.world>
2025-04-19 02:14:42 +0300

    feat: update CommandService for chat message handling
    Added necessary using directives for Minecraft links and players.
    Modified chat message sending method to use player.GetLink() for improved architecture.

--- src/Plugins/ProtocolSupport/Java/v1_13_to_1_20_1/Commands/CommandService.cs
+++ src/Plugins/ProtocolSupport/Java/v1_13_to_1_20_1/Commands/CommandService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Void.Minecraft.Links.Extensions;
 using Void.Minecraft.Network;
 using Void.Minecraft.Players;
 using Void.Minecraft.Players.Extensions;
@@ -6,6 +7,7 @@ using Void.Proxy.Api.Commands;
 using Void.Proxy.Api.Events;
 using Void.Proxy.Api.Events.Network;
 using Void.Proxy.Api.Events.Services;
+using Void.Proxy.Api.Players.Extensions;
 using Void.Proxy.Plugins.Common.Services.Commands;
 using Void.Proxy.Plugins.ProtocolSupport.Java.v1_13_to_1_20_1.Extensions;
 using Void.Proxy.Plugins.ProtocolSupport.Java.v1_13_to_1_20_1.Packets.Serverbound;
@@ -27,7 +29,7 @@ public class CommandService(ILogger<CommandService> logger, IEventService events
         if (!command.StartsWith('/'))
             command = "/" + command;
 
-        await player.SendPacketAsync(new ChatMessagePacket { Text = command }, cancellationToken);
+        await player.GetLink().SendPacketAsync(new ChatMessagePacket { Text = command }, cancellationToken);
         return true;
     }
 
