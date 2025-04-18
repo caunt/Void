da02773e25bc6c46b316f4c81b3d87b282a5c7e4
Vitalii Kotenko <gpg@caunt.world>
2025-04-18 04:24:41 +0300

    feat: add logging and player retrieval enhancements
    Added new `using` directives for dependency injection and logging in `PlayerServiceExtensions.cs`. Introduced `GetLogger` method to create a logger for players using their context services. Modified `TryGetByName` to include an output parameter for the player object, improving player service functionality.

--- src/Minecraft/Players/Extensions/PlayerServiceExtensions.cs
+++ src/Minecraft/Players/Extensions/PlayerServiceExtensions.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Void.Common.Players;
@@ -12,6 +15,18 @@ namespace Void.Minecraft.Players.Extensions;
 
 public static class PlayerServiceExtensions
 {
+    public static ILogger GetLogger(this IPlayer player)
+    {
+        var contextBuilder = new StringBuilder();
+        contextBuilder.Append(player.GetType().Name);
+        contextBuilder.Append(" <");
+        contextBuilder.Append(player);
+        contextBuilder.Append('>');
+
+        var loggerFactory = player.Context.Services.GetRequiredService<ILoggerFactory>();
+        return loggerFactory.CreateLogger(contextBuilder.ToString());
+    }
+
     public static bool TryGetByName(this IPlayerService players, string username, [NotNullWhen(true)] out IPlayer? player)
     {
         player = players.All.FirstOrDefault(player =>
