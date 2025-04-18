a6aa49e5d8317eec81844d0d8290124e60dc2afc
Vitalii Kotenko <gpg@caunt.world>
2025-04-18 07:17:32 +0300

    feat: instantiate example services from hosting abstractions, effectively using automatic registration of them as event listeners

--- src/Plugins/ExamplePlugin/ExamplePlugin.cs
+++ src/Plugins/ExamplePlugin/ExamplePlugin.cs
@@ -33,8 +33,8 @@ public class ExamplePlugin(ILogger<ExamplePlugin> logger, IDependencyService dep
         });
 
         // TODO: make them hosted?
-        dependencies.CreateInstance<InventoryService>();
-        dependencies.CreateInstance<ChatService>();
+        dependencies.GetRequiredService<InventoryService>();
+        dependencies.GetRequiredService<ChatService>();
     }
 
     [Subscribe]
