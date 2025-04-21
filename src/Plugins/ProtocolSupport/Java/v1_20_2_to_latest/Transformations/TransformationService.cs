bca74f638bf29df7774045da616e5b4190c4a9d4
Vitalii Kotenko <gpg@caunt.world>
2025-04-21 19:53:28 +0300

    feat: register text component transformations 1.21.4 => 1.21.5

--- src/Plugins/ProtocolSupport/Java/v1_20_2_to_latest/Transformations/TransformationService.cs
+++ src/Plugins/ProtocolSupport/Java/v1_20_2_to_latest/Transformations/TransformationService.cs
@@ -1,6 +1,10 @@
 ﻿using Void.Minecraft.Events;
+using Void.Minecraft.Network;
 using Void.Proxy.Api.Events;
+using Void.Proxy.Api.Network;
+using Void.Proxy.Plugins.Common.Extensions;
 using Void.Proxy.Plugins.Common.Services.Transformations;
+using Void.Proxy.Plugins.ProtocolSupport.Java.v1_20_2_to_latest.Packets.Clientbound;
 
 namespace Void.Proxy.Plugins.ProtocolSupport.Java.v1_20_2_to_latest.Transformations;
 
@@ -11,5 +15,10 @@ public class TransformationService : AbstractTransformationService
     {
         if (!Plugin.SupportedVersions.Contains(@event.Player.ProtocolVersion))
             return;
+
+        if (@event is not { Side: Side.Client, Phase: Phase.Play })
+            return;
+
+        @event.Player.RegisterSystemTransformations<SystemChatMessagePacket>(SystemChatMessagePacket.Transformations);
     }
 }
