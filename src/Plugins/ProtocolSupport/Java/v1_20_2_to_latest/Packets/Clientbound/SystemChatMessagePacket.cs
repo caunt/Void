35c478f9aab5f3cd0efd6cc138340e6567a1bc03
Vitalii Kotenko <gpg@caunt.world>
2025-04-21 19:53:43 +0300

    feat: text component transformations 1.21.4 => 1.21.5

--- src/Plugins/ProtocolSupport/Java/v1_20_2_to_latest/Packets/Clientbound/SystemChatMessagePacket.cs
+++ src/Plugins/ProtocolSupport/Java/v1_20_2_to_latest/Packets/Clientbound/SystemChatMessagePacket.cs
@@ -1,12 +1,29 @@
 ﻿using Void.Minecraft.Buffers;
 using Void.Minecraft.Components.Text;
+using Void.Minecraft.Components.Text.Transformers;
 using Void.Minecraft.Network;
 using Void.Minecraft.Network.Messages.Packets;
+using Void.Minecraft.Network.Registries.Transformations.Mappings;
+using Void.Minecraft.Network.Registries.Transformations.Properties;
 
 namespace Void.Proxy.Plugins.ProtocolSupport.Java.v1_20_2_to_latest.Packets.Clientbound;
 
 public class SystemChatMessagePacket : IMinecraftClientboundPacket<SystemChatMessagePacket>
 {
+    public static MinecraftPacketTransformationMapping[] Transformations { get; } = [
+        new(ProtocolVersion.MINECRAFT_1_21_4, ProtocolVersion.MINECRAFT_1_21_5, wrapper =>
+        {
+            Console.WriteLine("111");
+            ComponentNbtTransformers.Passthrough_v1_21_4_to_v1_21_5(wrapper);
+            wrapper.Passthrough<BoolProperty>();
+        }),
+        new(ProtocolVersion.MINECRAFT_1_21_5, ProtocolVersion.MINECRAFT_1_21_4, wrapper =>
+        {
+            ComponentNbtTransformers.Passthrough_v1_21_5_to_v1_21_4(wrapper);
+            wrapper.Passthrough<BoolProperty>();
+        })
+    ];
+
     public required Component Message { get; set; }
     public required bool Overlay { get; set; }
 
