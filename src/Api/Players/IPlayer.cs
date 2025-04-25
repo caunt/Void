32ec5adb47841baeb2ad893c5793ce072c5cf4eb
Vitalii Kotenko <gpg@caunt.world>
2025-04-25 23:11:35 +0300

    feat: stable hash codes for IPlayer
    The `IPlayer` interface now implements `IEquatable<IPlayer>`, enabling equality comparisons between instances. A new method, `GetStableHashCode`, has been added to compute a hash code based on the `Client` property, complete with XML documentation detailing its functionality.

--- src/Api/Players/IPlayer.cs
+++ src/Api/Players/IPlayer.cs
@@ -4,9 +4,19 @@ using Void.Proxy.Api.Players.Contexts;
 
 namespace Void.Proxy.Api.Players;
 
-public interface IPlayer : ICommandSource, IAsyncDisposable, IDisposable
+public interface IPlayer : IEquatable<IPlayer>, ICommandSource, IAsyncDisposable, IDisposable
 {
     public TcpClient Client { get; }
     public string RemoteEndPoint { get; }
     public IPlayerContext Context { get; }
+
+    /// <summary>
+    /// Computes a stable hash code for the current instance. This is useful when the instance is upgraded or replaced to another implementation.
+    /// </summary>
+    /// <remarks>The hash code is based on the <see cref="Client"/> property.</remarks>
+    /// <returns>An integer representing the hash code of the current instance, derived from the <see cref="Client"/> property.</returns>
+    public int GetStableHashCode()
+    {
+        return Client.GetHashCode();
+    }
 }
