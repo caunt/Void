f2a14932752db501fb92fd1f2f44c7a9426d7cc5
Vitalii Kotenko <gpg@caunt.world>
2025-04-27 00:40:43 +0300

    feat: extension methods for synchronizing collections
    Introduces a new static class `EnumerableExtensions` in the `Void.Proxy.Api.Extensions` namespace. This class provides two extension methods for `IEnumerable<T>`: `Synchronized` and `SynchronizedAsync`. The `Synchronized` method ensures thread-safe iteration using an `AsyncLock` and supports cancellation via a `CancellationToken`. The `SynchronizedAsync` method allows for asynchronous iteration, enabling non-blocking operations while checking for cancellation requests.

---
+++ src/Api/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.CompilerServices;
+using Nito.AsyncEx;
+
+namespace Void.Proxy.Api.Extensions;
+
+public static class EnumerableExtensions
+{
+    public static IEnumerable<T> Synchronized<T>(this IEnumerable<T> source, AsyncLock @lock, CancellationToken cancellationToken)
+    {
+        using var _ = @lock.Lock(cancellationToken);
+
+        foreach (var item in source)
+        {
+            if (cancellationToken.IsCancellationRequested)
+                yield break;
+
+            yield return item;
+        }
+    }
+
+    public static async IAsyncEnumerable<T> SynchronizedAsync<T>(this IEnumerable<T> source, AsyncLock @lock, [EnumeratorCancellation] CancellationToken cancellationToken)
+    {
+        using var _ = await @lock.LockAsync(cancellationToken);
+
+        foreach (var item in source)
+        {
+            if (cancellationToken.IsCancellationRequested)
+                yield break;
+
+            yield return item;
+        }
+    }
+}
