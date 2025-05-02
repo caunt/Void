2645c1eb7974f94716243d1602afd94630cd8eb5
Vitalii Kotenko <gpg@caunt.world>
2025-05-02 22:36:22 +0300

    chore: reproduce docfx bug in playground

--- src/Playground/Program.cs
+++ src/Playground/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace Void.Playground;
+
+// Bug: https://github.com/dotnet/docfx/issues/10655
+// Bug: docfx docfx/docfx.json --serve
+
+public ref struct TestStruct;
+
+public static class TestClass
+{
+    public static byte TestMethod<TRefStruct>(this TRefStruct value) where TRefStruct : allows ref struct => 1;
+}
