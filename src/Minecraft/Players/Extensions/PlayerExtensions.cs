680a580486d19b9548bc347f47d24ae3b6c266fa
Vitalii Kotenko <gpg@caunt.world>
2025-05-02 18:13:25 +0300

    fix: temporary rollback C# 14 extension type

--- src/Minecraft/Players/Extensions/PlayerExtensions.cs
+++ src/Minecraft/Players/Extensions/PlayerExtensions.cs
@@ -10,29 +10,50 @@ namespace Void.Minecraft.Players.Extensions;
 
 public static class PlayerExtensions
 {
-    extension(IPlayer player)
+    // extension(IPlayer player)
+    // {
+    //     public IMinecraftPlayer AsMinecraft => TryGetMinecraftPlayer(player, out var minecraftPlayer) ? minecraftPlayer : throw new InvalidOperationException($"Player is not a {nameof(IMinecraftPlayer)}.");
+    // 
+    //     public IMinecraftPlayer AsMinecraftPlayer()
+    //     {
+    //         return player.AsMinecraft;
+    //     }
+    // 
+    //     public async ValueTask KickAsync(Component? reason = null, CancellationToken cancellationToken = default)
+    //     {
+    //         var players = player.Context.Services.GetRequiredService<IPlayerService>();
+    // 
+    //         if (player.TryGetMinecraftPlayer(out var minecraftPlayer))
+    //             await players.KickPlayerAsync(minecraftPlayer, reason, cancellationToken);
+    //         else
+    //             await players.KickPlayerAsync(player, reason?.SerializeLegacy(), cancellationToken);
+    //     }
+    // 
+    //     public bool TryGetMinecraftPlayer([MaybeNullWhen(false)] out IMinecraftPlayer minecraftPlayer)
+    //     {
+    //         minecraftPlayer = player as IMinecraftPlayer;
+    //         return minecraftPlayer is not null;
+    //     }
+    // }
+
+    public static IMinecraftPlayer AsMinecraftPlayer(this IPlayer player)
     {
-        public IMinecraftPlayer AsMinecraft => TryGetMinecraftPlayer(player, out var minecraftPlayer) ? minecraftPlayer : throw new InvalidOperationException($"Player is not a {nameof(IMinecraftPlayer)}.");
+        return TryGetMinecraftPlayer(player, out var minecraftPlayer) ? minecraftPlayer : throw new InvalidOperationException($"Player is not a {nameof(IMinecraftPlayer)}.");
+    }
 
-        public IMinecraftPlayer AsMinecraftPlayer()
-        {
-            return player.AsMinecraft;
-        }
+    public static async ValueTask KickAsync(this IPlayer player, Component? reason = null, CancellationToken cancellationToken = default)
+    {
+        var players = player.Context.Services.GetRequiredService<IPlayerService>();
 
-        public async ValueTask KickAsync(Component? reason = null, CancellationToken cancellationToken = default)
-        {
-            var players = player.Context.Services.GetRequiredService<IPlayerService>();
+        if (player.TryGetMinecraftPlayer(out var minecraftPlayer))
+            await players.KickPlayerAsync(minecraftPlayer, reason, cancellationToken);
+        else
+            await players.KickPlayerAsync(player, reason?.SerializeLegacy(), cancellationToken);
+    }
 
-            if (player.TryGetMinecraftPlayer(out var minecraftPlayer))
-                await players.KickPlayerAsync(minecraftPlayer, reason, cancellationToken);
-            else
-                await players.KickPlayerAsync(player, reason?.SerializeLegacy(), cancellationToken);
-        }
-
-        public bool TryGetMinecraftPlayer([MaybeNullWhen(false)] out IMinecraftPlayer minecraftPlayer)
-        {
-            minecraftPlayer = player as IMinecraftPlayer;
-            return minecraftPlayer is not null;
-        }
+    public static bool TryGetMinecraftPlayer(this IPlayer player, [MaybeNullWhen(false)] out IMinecraftPlayer minecraftPlayer)
+    {
+        minecraftPlayer = player as IMinecraftPlayer;
+        return minecraftPlayer is not null;
     }
 }
