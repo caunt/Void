16406c752ecdc3c5271d28db090ac6aaa071c05b
Vitalii Kotenko <gpg@caunt.world>
2025-05-20 20:36:36 +0300

    chore: move command registration into chat service

--- src/Plugins/ExamplePlugin/ExamplePlugin.cs
+++ src/Plugins/ExamplePlugin/ExamplePlugin.cs
@@ -1,13 +1,8 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Void.Minecraft.Commands.Brigadier;
-using Void.Minecraft.Commands.Brigadier.Context;
-using Void.Minecraft.Commands.Brigadier.Extensions;
-using Void.Proxy.Api.Commands;
 using Void.Proxy.Api.Events;
 using Void.Proxy.Api.Events.Plugins;
 using Void.Proxy.Api.Events.Proxy;
-using Void.Proxy.Api.Players;
 using Void.Proxy.Api.Plugins;
 using Void.Proxy.Api.Plugins.Dependencies;
 using Void.Proxy.Plugins.ExamplePlugin.Services;
@@ -17,7 +12,7 @@ namespace Void.Proxy.Plugins.ExamplePlugin;
 // Implementing IPlugin makes class an entry point to your plugin
 // Constructor arguments are used to inject many API services implemented by proxy and other plugins
 // See ../Services/ directory for more examples
-public class ExamplePlugin(ILogger logger, IDependencyService dependencies, ICommandService commands) : IPlugin
+public class ExamplePlugin(ILogger logger, IDependencyService dependencies) : IPlugin
 {
     public string Name => nameof(ExamplePlugin);
 
@@ -38,31 +33,6 @@ public class ExamplePlugin(ILogger logger, IDependencyService dependencies, ICom
             // Scoped services are instantiated per-player
             services.AddScoped<InventoryService>();
         });
-
-        // Register your commands in brigadier-like way
-        // https://github.com/Mojang/brigadier/
-        commands.Register(builder => builder
-            .Literal("slot")
-            .Executes(SlotCommandAsync)
-            .Then(builder => builder
-                .Argument("index", Arguments.Integer())
-                .Executes(SlotCommandAsync)));
-
-        async ValueTask<int> SlotCommandAsync(CommandContext context, CancellationToken cancellationToken)
-        {
-            // Commands might be triggered by console, plugins, players, or anything
-            if (context.Source is not IPlayer player)
-            {
-                logger.LogInformation("This command can be executed only by player");
-                return 1;
-            }
-
-            // You can resolve services manually from player context
-            var inventory = player.Context.Services.GetRequiredService<InventoryService>();
-            var result = await inventory.ChangeSlotAsync(context, cancellationToken);
-
-            return result;
-        }
     }
 
     [Subscribe]
--- src/Plugins/ExamplePlugin/Services/ChatService.cs
+++ src/Plugins/ExamplePlugin/Services/ChatService.cs
@@ -1,17 +1,32 @@
-﻿using Void.Minecraft.Events;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Void.Minecraft.Commands.Brigadier;
+using Void.Minecraft.Commands.Brigadier.Context;
+using Void.Minecraft.Commands.Brigadier.Extensions;
+using Void.Minecraft.Events;
 using Void.Minecraft.Network;
 using Void.Minecraft.Players.Extensions;
+using Void.Proxy.Api.Commands;
 using Void.Proxy.Api.Configurations;
 using Void.Proxy.Api.Events;
+using Void.Proxy.Api.Events.Plugins;
 using Void.Proxy.Api.Events.Proxy;
 using Void.Proxy.Api.Network;
+using Void.Proxy.Api.Players;
 using Void.Proxy.Api.Players.Extensions;
 
 namespace Void.Proxy.Plugins.ExamplePlugin.Services;
 
-// Here you can use DI to inject any service API you want to use.
-public class ChatService(IConfigurationService configs) : IEventListener
+// Example Chat configuration class.
+public class ChatSettings
 {
+    public string WelcomeMessage { get; set; } = $"Welcome to the %SERVER% server!";
+}
+
+// Here you can use DI to inject any service API you want to use.
+public class ChatService(ILogger<ChatService> logger, IConfigurationService configs, ICommandService commands) : IEventListener
+{
+    // Save settings instance locally, it is safe and will be updated in place if changes are made on disk.
     private ChatSettings? _settings;
 
     [Subscribe]
@@ -46,9 +61,40 @@ public class ChatService(IConfigurationService configs) : IEventListener
 
         await @event.Player.SendChatMessageAsync(message, cancellationToken);
     }
-}
 
-public class ChatSettings
-{
-    public string WelcomeMessage { get; set; } = $"Welcome to the %SERVER% server!";
+    // Registering commands and interaction with scoped services.
+    [Subscribe]
+    public void OnPluginLoading(PluginLoadingEvent @event)
+    {
+        // This event is fired when any plugin is being loaded
+
+        // Skip all other plugins load events except ours
+        if (@event.Plugin != this)
+            return;
+
+        // Register your commands in brigadier-like way
+        // https://github.com/Mojang/brigadier/
+        commands.Register(builder => builder
+            .Literal("slot")
+            .Executes(SlotCommandAsync)
+            .Then(builder => builder
+                .Argument("index", Arguments.Integer())
+                .Executes(SlotCommandAsync)));
+
+        async ValueTask<int> SlotCommandAsync(CommandContext context, CancellationToken cancellationToken)
+        {
+            // Commands might be triggered by console, plugins, players, or anything
+            if (context.Source is not IPlayer player)
+            {
+                logger.LogInformation("This command can be executed only by player");
+                return 1;
+            }
+
+            // You can resolve scoped services manually from player context
+            var inventory = player.Context.Services.GetRequiredService<InventoryService>();
+            var result = await inventory.ChangeSlotAsync(context, cancellationToken);
+
+            return result;
+        }
+    }
 }
