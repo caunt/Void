b2b547075bfca28fd280bfdf545302ad119c7dd5
Vitalii Kotenko <gpg@caunt.world>
2025-05-22 22:28:43 +0300

    docs: first version of downloads page

--- docs/astro/src/content.config.ts
+++ docs/astro/src/content.config.ts
@@ -3,5 +3,20 @@ import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
 
 export const collections = {
-	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+    docs: defineCollection({
+        loader: docsLoader(),
+        schema: docsSchema()
+    }),
+    releases: defineCollection({
+        loader: async () => {
+            const response = await fetch("https://api.github.com/repos/caunt/Void/releases");
+            const data = await response.json();
+
+            data.forEach((item : any) => {
+                item.id = item.id.toString();
+            });
+
+            return data;
+        }
+    })
 };
