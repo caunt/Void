69687a2eea63465ef5b78ac2794603faf80e4c5e
Vitalii Kotenko <caunt.official@gmail.com>
2025-06-30 03:39:54 +0300

    test: add comprehensive Guid and Uuid tests

---
+++ tests/GuidTests/GuidHelperTests.cs
@@ -0,0 +1,28 @@
+using System;
+using Void.Proxy.Utils;
+using Xunit;
+
+namespace Void.Tests;
+
+public class GuidHelperTests
+{
+    [Fact]
+    public void FromStringHash_GeneratesConsistentGuid()
+    {
+        const string input = "test";
+        var guid = GuidHelper.FromStringHash(input);
+        Assert.Equal(guid, GuidHelper.FromStringHash(input));
+        Assert.Equal(3, GuidHelper.GetVersion(guid));
+        Assert.Equal(1, GuidHelper.GetVariant(guid));
+    }
+
+    [Fact]
+    public void FromLongs_ConstructsExpectedGuid()
+    {
+        const long mostSig = 0x1122334455667788L;
+        const long leastSig = unchecked((long)0x99AABBCCDDEEFF00UL);
+        var guid = GuidHelper.FromLongs(mostSig, leastSig);
+        Assert.Equal(guid, GuidHelper.FromLongs(mostSig, leastSig));
+    }
+}
+
