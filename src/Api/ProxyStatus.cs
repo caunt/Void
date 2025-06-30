85987e21d4973cb1510ab3a594068edcb89cfc30
Vitalii Kotenko <caunt.official@gmail.com>
2025-06-30 15:37:53 +0300

    docs(api): add xml docs to ProxyStatus

--- src/Api/ProxyStatus.cs
+++ src/Api/ProxyStatus.cs
@@ -1,8 +1,22 @@
 ﻿namespace Void.Proxy.Api;
 
+/// <summary>
+/// Represents the operational state of the proxy server.
+/// </summary>
 public enum ProxyStatus
 {
+    /// <summary>
+    /// The proxy is running and accepting connections.
+    /// </summary>
     Alive,
+
+    /// <summary>
+    /// The proxy has temporarily paused accepting new connections.
+    /// </summary>
     Paused,
+
+    /// <summary>
+    /// The proxy is in the process of shutting down.
+    /// </summary>
     Stopping
 }
