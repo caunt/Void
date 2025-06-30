d8abb93d1de7871d09ff2f6d0e45760847c469c1
Vitalii Kotenko <caunt.official@gmail.com>
2025-06-30 15:35:06 +0300

    docs(api): add documentation for IServerService

--- src/Api/Servers/IServerService.cs
+++ src/Api/Servers/IServerService.cs
@@ -1,6 +1,12 @@
 ﻿namespace Void.Proxy.Api.Servers;
 
+/// <summary>
+/// Provides access to the collection of servers available to the proxy.
+/// </summary>
 public interface IServerService
 {
+    /// <summary>
+    /// Gets all configured servers.
+    /// </summary>
     public IEnumerable<IServer> All { get; }
 }
