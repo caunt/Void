b203399364add67cca337a179ae17c290b3bf081
Vitalii Kotenko <caunt.official@gmail.com>
2025-06-30 05:03:48 +0300

    docs(api): document IServer

--- src/Api/Servers/IServer.cs
+++ src/Api/Servers/IServer.cs
@@ -2,12 +2,34 @@
 
 namespace Void.Proxy.Api.Servers;
 
+/// <summary>
+/// Represents a server that players can connect to.
+/// </summary>
 public interface IServer
 {
+    /// <summary>
+    /// Gets the name of the server.
+    /// </summary>
     public string Name { get; }
+
+    /// <summary>
+    /// Gets the host address of the server.
+    /// </summary>
     public string Host { get; }
+
+    /// <summary>
+    /// Gets the port used for connecting to the server.
+    /// </summary>
     public int Port { get; }
+
+    /// <summary>
+    /// Gets or sets the implementation brand of the server.
+    /// </summary>
     public string? Brand { get; set; }
 
+    /// <summary>
+    /// Creates a <see cref="TcpClient"/> connected to this server.
+    /// </summary>
+    /// <returns>A configured <see cref="TcpClient"/> instance.</returns>
     public TcpClient CreateTcpClient() => new(Host, Port);
 }
