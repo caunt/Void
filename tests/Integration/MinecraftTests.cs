b6eea22dbb54879eb071cebfbd1c057f737109c5
Vitalii Kotenko <caunt.official@gmail.com>
2025-07-13 05:20:49 +0300

    test: increase task timeout

--- tests/Integration/MinecraftTests.cs
+++ tests/Integration/MinecraftTests.cs
@@ -66,7 +66,7 @@ public class MinecraftTests : IDisposable
 
         try
         {
-            using var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(60));
+            using var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(120));
 
             var paperJarPath = await SetupPaperServerAsync(cancellationTokenSource.Token);
             var minecraftConsoleClientExecutablePath = await SetupMinecraftConsoleClientAsync(cancellationTokenSource.Token);
