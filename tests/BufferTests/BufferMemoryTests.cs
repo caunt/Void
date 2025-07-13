8cf7569c7425e2ed82c772f2c47539913c5ce54d
Vitalii Kotenko <caunt.official@gmail.com>
2025-07-13 14:10:34 +0300

    test: expand BufferMemory and BufferSpan tests

--- tests/BufferTests/BufferMemoryTests.cs
+++ tests/BufferTests/BufferMemoryTests.cs
@@ -16,6 +16,40 @@ public class BufferMemoryTests
     }
 
     [Fact]
+    public void Slice_WholeBuffer_ReturnsIdenticalBytes()
+    {
+        var data = new byte[] { 1, 2, 3 };
+        var memory = new BufferMemory(data);
+
+        var slice = memory.Slice(0, data.Length);
+
+        Assert.Equal(data, slice.Span.Access(0, data.Length).ToArray());
+    }
+
+    [Fact]
+    public void Slice_Modifications_AffectSource()
+    {
+        var data = new byte[] { 1, 2, 3, 4 };
+        var memory = new BufferMemory(data);
+
+        var slice = memory.Slice(1, 2);
+        var span = slice.Span.Access(0, 2);
+        span[0] = 9;
+
+        Assert.Equal(9, memory.Span.Access(1, 2)[0]);
+    }
+
+    [Fact]
+    public void Slice_ZeroLength_ReturnsEmptySpan()
+    {
+        var memory = new BufferMemory(new byte[] { 1, 2 });
+
+        var slice = memory.Slice(1, 0);
+
+        Assert.Equal(0, slice.Span.Length);
+    }
+
+    [Fact]
     public void Slice_NegativePosition_Throws()
     {
         var memory = new BufferMemory(new byte[10]);
--- tests/BufferTests/BufferSpanTests.cs
+++ tests/BufferTests/BufferSpanTests.cs
@@ -37,6 +37,26 @@ public class BufferSpanTests
     }
 
     [Fact]
+    public void Length_ReturnsSourceLength()
+    {
+        var span = new BufferSpan(stackalloc byte[7]);
+
+        Assert.Equal(7, span.Length);
+    }
+
+    [Fact]
+    public void Remaining_UpdatesWithPosition()
+    {
+        var span = new BufferSpan(stackalloc byte[5]);
+
+        Assert.Equal(5, span.Remaining);
+
+        span.Position = 2;
+
+        Assert.Equal(3, span.Remaining);
+    }
+
+    [Fact]
     public void Slice_ReturnsExpectedSpan()
     {
         Span<byte> source = stackalloc byte[] { 0, 1, 2, 3, 4 };
@@ -46,12 +66,52 @@ public class BufferSpanTests
     }
 
     [Fact]
+    public void Slice_Modifications_AffectSource()
+    {
+        Span<byte> source = stackalloc byte[] { 0, 1, 2, 3 };
+        var span = new BufferSpan(source);
+
+        var slice = span.Slice(1, 2);
+        slice.Access(0, 2)[0] = 9;
+
+        Assert.Equal(9, span.Access(1, 2)[0]);
+    }
+
+    [Fact]
+    public void Slice_ZeroLength_ReturnsEmptySpan()
+    {
+        Span<byte> source = stackalloc byte[] { 0, 1 };
+        var span = new BufferSpan(source);
+
+        var slice = span.Slice(1, 0);
+
+        Assert.Equal(0, slice.Length);
+    }
+
+    [Fact]
     public void Access_ByLength_UsesCurrentPosition()
     {
         Span<byte> source = stackalloc byte[] { 0, 1, 2, 3, 4 };
         var span = new BufferSpan(source);
         span.Position = 2;
         Assert.Equal(new byte[] { 2, 3 }, span.Access(2).ToArray());
+    }
+
+    [Fact]
+    public void Access_ZeroLength_ReturnsEmpty()
+    {
+        var span = new BufferSpan(stackalloc byte[5]);
+
+        Assert.Equal(0, span.Access(0).Length);
+    }
+
+    [Fact]
+    public void Access_WholeSpan_ReturnsAllBytes()
+    {
+        Span<byte> source = stackalloc byte[] { 1, 2, 3 };
+        var span = new BufferSpan(source);
+
+        Assert.Equal(new byte[] { 1, 2, 3 }, span.Access(0, 3).ToArray());
     }
 
     [Fact]
@@ -112,11 +172,54 @@ public class BufferSpanTests
     }
 
     [Fact]
+    public void Seek_ZeroOffset_DoesNotChangePosition()
+    {
+        var span = new BufferSpan(stackalloc byte[10]);
+        span.Position = 4;
+
+        span.Seek(0, SeekOrigin.Current);
+
+        Assert.Equal(4, span.Position);
+    }
+
+    [Fact]
+    public void Seek_EndWithZeroOffset_SetsToEnd()
+    {
+        var span = new BufferSpan(stackalloc byte[10]);
+
+        span.Seek(0, SeekOrigin.End);
+
+        Assert.Equal(10, span.Position);
+    }
+
+    [Fact]
+    public void Seek_BeginWithZeroOffset_SetsToStart()
+    {
+        var span = new BufferSpan(stackalloc byte[10]);
+        span.Position = 5;
+
+        span.Seek(0, SeekOrigin.Begin);
+
+        Assert.Equal(0, span.Position);
+    }
+
+    [Fact]
     public void Seek_End_UpdatesPosition()
     {
         var span = new BufferSpan(stackalloc byte[10]);
         span.Seek(-2, SeekOrigin.End);
         Assert.Equal(8, span.Position);
+    }
+
+    [Fact]
+    public void Seek_CurrentNegativeOffsetWithinBounds_UpdatesPosition()
+    {
+        var span = new BufferSpan(stackalloc byte[10]);
+        span.Position = 5;
+
+        span.Seek(-3, SeekOrigin.Current);
+
+        Assert.Equal(2, span.Position);
     }
 
     [Fact]
