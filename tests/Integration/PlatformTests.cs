e5510a15f311d3fca8fe09489ede545cca8d9889
Vitalii Kotenko <gpg@caunt.world>
2025-07-17 03:31:10 +0300

    test: add integration tests for EntryPoint
    Added a new test class `PlatformTests` in the `Void.Tests.Integration` namespace. This includes the test method `EntryPoint_RunsStopsSuccessfuly`, which verifies that the `EntryPoint.RunAsync` function completes successfully within a 10-second timeout and asserts that the exit code is 0.

---
+++ tests/Integration/PlatformTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Void.Proxy;
+using Xunit;
+
+namespace Void.Tests.Integration;
+
+public class PlatformTests
+{
+    [Fact]
+    public async Task EntryPoint_RunsStopsSuccessfuly()
+    {
+        using var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+        var exitCode = await EntryPoint.RunAsync(cancellationTokenSource.Token);
+
+        Assert.Equal(0, exitCode);
+    }
+}
