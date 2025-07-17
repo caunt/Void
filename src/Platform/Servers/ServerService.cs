3aa41c24ef91c42ae72ba48ae5c61ca8a1048dd7
Vitalii Kotenko <caunt.official@gmail.com>
2025-07-17 05:58:12 +0300

    feat(platform): add ignore-file-servers option

--- src/Platform/Servers/ServerService.cs
+++ src/Platform/Servers/ServerService.cs
@@ -1,10 +1,21 @@
-﻿using Void.Proxy.Api.Links;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using Void.Proxy.Api.Links;
 using Void.Proxy.Api.Servers;
 using Void.Proxy.Api.Settings;
 
 namespace Void.Proxy.Servers;
 
-public class ServerService(ISettings settings, ILinkService links) : IServerService
+public class ServerService(ISettings settings, ILinkService links, InvocationContext context) : IServerService
 {
-    public IEnumerable<IServer> All => settings.Servers.Concat(links.All.Select(link => link.Server));
+    private static readonly Option<bool> _ignoreFileServersOption = new("--ignore-file-servers", description: "Ignore servers specified in configuration files");
+
+    public static void RegisterOptions(Command command)
+    {
+        command.AddOption(_ignoreFileServersOption);
+    }
+
+    public IEnumerable<IServer> All
+        => (context.ParseResult.GetValueForOption(_ignoreFileServersOption) ? [] : settings.Servers)
+            .Concat(links.All.Select(link => link.Server));
 }
