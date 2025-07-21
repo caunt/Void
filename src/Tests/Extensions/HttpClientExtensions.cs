d28410d6b038f013f33f857a8e83385f0a0c5fe4
Vitalii Kotenko <caunt.official@gmail.com>
2025-07-21 10:52:47 +0300

    test(extensions): 🔁 add retry logic

--- src/Tests/Extensions/HttpClientExtensions.cs
+++ src/Tests/Extensions/HttpClientExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,10 +10,23 @@ public static class HttpClientExtensions
 {
     public static async Task DownloadFileAsync(this HttpClient client, string url, string destination, CancellationToken cancellationToken)
     {
-        using var response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead, cancellationToken = default);
-        response.EnsureSuccessStatusCode();
 
-        await using var fileStream = File.Create(destination);
-        await response.Content.CopyToAsync(fileStream, cancellationToken);
+        for (var attempt = 0; attempt < 3; attempt++)
+        {
+            try
+            {
+                using var response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead, cancellationToken);
+                response.EnsureSuccessStatusCode();
+
+                await using var fileStream = File.Create(destination);
+                await response.Content.CopyToAsync(fileStream, cancellationToken);
+
+                return;
+            }
+            catch (HttpRequestException) when (attempt < 2)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+            }
+        }
     }
 }
