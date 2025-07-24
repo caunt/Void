e86db88499afdd6f52e2a8ff09eb70a89faa4db7
caunt <gpg@caunt.world>
2025-07-24 06:57:30 +0300

    fix(packets): 📝 temporary disable SystemChatMessagePacket transformations
    Removed unused using directives and commented out the Transformations array in SystemChatMessagePacket. Added TODO comments to indicate that packet upgrade logic is pending implementation and outlined necessary steps for future development.

--- src/Plugins/ProtocolSupport/Java/v1_20_2_to_latest/Packets/Clientbound/SystemChatMessagePacket.cs
+++ src/Plugins/ProtocolSupport/Java/v1_20_2_to_latest/Packets/Clientbound/SystemChatMessagePacket.cs
@@ -1,26 +1,44 @@
 ﻿using Void.Minecraft.Buffers;
 using Void.Minecraft.Components.Text;
-using Void.Minecraft.Components.Text.Transformers;
 using Void.Minecraft.Network;
 using Void.Minecraft.Network.Messages.Packets;
 using Void.Minecraft.Network.Registries.Transformations.Mappings;
-using Void.Minecraft.Network.Registries.Transformations.Properties;
 
 namespace Void.Proxy.Plugins.ProtocolSupport.Java.v1_20_2_to_latest.Packets.Clientbound;
 
 public class SystemChatMessagePacket : IMinecraftClientboundPacket<SystemChatMessagePacket>
 {
     public static MinecraftPacketTransformationMapping[] Transformations { get; } = [
-        new(ProtocolVersion.MINECRAFT_1_21_4, ProtocolVersion.MINECRAFT_1_21_5, wrapper =>
-        {
-            ComponentNbtTransformers.Passthrough_v1_21_4_to_v1_21_5(wrapper);
-            wrapper.Passthrough<BoolProperty>();
-        }),
-        new(ProtocolVersion.MINECRAFT_1_21_5, ProtocolVersion.MINECRAFT_1_21_4, wrapper =>
-        {
-            ComponentNbtTransformers.Passthrough_v1_21_5_to_v1_21_4(wrapper);
-            wrapper.Passthrough<BoolProperty>();
-        })
+        // TODO - upgrade packet here, not rely on protocolVersion below
+        //
+        // new(ProtocolVersion.MINECRAFT_1_20_2, ProtocolVersion.MINECRAFT_1_20_3, wrapper =>
+        // {
+        //     var componentJson = wrapper.Read<StringProperty>();
+        //     var node = JsonNode.Parse(componentJson.AsPrimitive) ??
+        //         throw new InvalidOperationException("Failed to parse component JSON");
+        // 
+        //     var component = Component.DeserializeJson(node);
+        //     wrapper.Write(NamedNbtProperty.FromNbtTag(component.SerializeNbt()));
+        // 
+        //     wrapper.Passthrough<BoolProperty>();
+        // }),
+        // new(ProtocolVersion.MINECRAFT_1_20_3, ProtocolVersion.MINECRAFT_1_20_2, wrapper =>
+        // {
+        //     ComponentNbtTransformers.Passthrough_v1_20_3_to_v1_20_2(wrapper);
+        //     wrapper.Passthrough<BoolProperty>();
+        // }),
+
+        // TODO - Required but will not work until upgrades above are done
+        // new(ProtocolVersion.MINECRAFT_1_21_4, ProtocolVersion.MINECRAFT_1_21_5, wrapper =>
+        // {
+        //     ComponentNbtTransformers.Passthrough_v1_21_4_to_v1_21_5(wrapper);
+        //     wrapper.Passthrough<BoolProperty>();
+        // }),
+        // new(ProtocolVersion.MINECRAFT_1_21_5, ProtocolVersion.MINECRAFT_1_21_4, wrapper =>
+        // {
+        //     ComponentNbtTransformers.Passthrough_v1_21_5_to_v1_21_4(wrapper);
+        //     wrapper.Passthrough<BoolProperty>();
+        // })
     ];
 
     public required Component Message { get; set; }
