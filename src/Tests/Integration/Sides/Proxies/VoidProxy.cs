c7c4ab72507aacd78866cd2fb880b5e2791b844a
caunt <gpg@caunt.world>
2025-07-25 19:02:55 +0300

    test(integration): ✨ enhance async handling in CreateAsync
    - Updated `using` directives to include LINQ and async features.
    - Changed `CreateAsync` to be an async method, allowing for better asynchronous operations.
    - Modified the method to await the `EntryPoint.RunAsync` task.
    - Added a loop to wait for the proxy to start, improving initialization handling.
    - Updated the return statement to create a new `VoidProxy` instance with the awaited task.

--- src/Tests/Integration/Sides/Proxies/VoidProxy.cs
+++ src/Tests/Integration/Sides/Proxies/VoidProxy.cs
@@ -2,6 +2,7 @@ namespace Void.Tests.Integration.Sides.Proxies;
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Void.Proxy;
@@ -22,7 +23,7 @@ public class VoidProxy : IIntegrationSide
         _cancellationTokenSource = cancellationTokenSource;
     }
 
-    public static Task<VoidProxy> CreateAsync(string targetServer, int proxyPort, bool ignoreFileServers = true, bool offlineMode = true, CancellationToken cancellationToken = default)
+    public static async Task<VoidProxy> CreateAsync(string targetServer, int proxyPort, bool ignoreFileServers = true, bool offlineMode = true, CancellationToken cancellationToken = default)
     {
         var logWriter = new CollectingTextWriter();
         var cancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
@@ -41,7 +42,13 @@ public class VoidProxy : IIntegrationSide
         if (offlineMode)
             args.Add("--offline");
 
-        return Task.FromResult(new VoidProxy(logWriter, EntryPoint.RunAsync(logWriter: logWriter, cancellationToken: cancellationToken, args: [.. args]), cancellationTokenSource));
+        var task = EntryPoint.RunAsync(logWriter: logWriter, cancellationToken: cancellationToken, args: [.. args]);
+
+        // Wait for the proxy to start, because it takes some time to listen on the port
+        while (logWriter.Lines.All(line => !line.Contains("Proxy started")))
+            await Task.Delay(1_000, cancellationToken);
+
+        return new VoidProxy(logWriter, task, cancellationTokenSource);
     }
 
     public async ValueTask DisposeAsync()
