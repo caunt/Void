5933884b3e62bfd5a54f2437c6e379c50c03bcea
Vitalii Kotenko <caunt.official@gmail.com>
2025-08-04 23:15:19 +0300

    fix(minecraft): 🐛 guard against negative list size

--- src/Minecraft/Network/Registries/Transformations/Properties/ListProperty.cs
+++ src/Minecraft/Network/Registries/Transformations/Properties/ListProperty.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Void.Minecraft.Buffers;
 
 namespace Void.Minecraft.Network.Registries.Transformations.Properties;
@@ -12,6 +13,9 @@ public record ListProperty<TPacketProperty>(List<TPacketProperty> Values) : IPac
 
     public static ListProperty<TPacketProperty> Read(ref MinecraftBuffer buffer, int size)
     {
+        if (size < 0)
+            throw new ArgumentOutOfRangeException(nameof(size));
+
         if (size == 0)
             return new([]);
 
