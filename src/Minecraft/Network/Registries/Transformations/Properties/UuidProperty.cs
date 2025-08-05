ee5e4ed635893911f807077c7c0e8514f13bc051
Vitalii Kotenko <caunt.official@gmail.com>
2025-08-05 23:12:26 +0300

    perf(minecraft): ⚡ reduce allocations in UuidProperty

--- src/Minecraft/Network/Registries/Transformations/Properties/UuidProperty.cs
+++ src/Minecraft/Network/Registries/Transformations/Properties/UuidProperty.cs
@@ -1,5 +1,4 @@
 using System;
-using System.IO;
 using Void.Minecraft.Buffers;
 using Void.Minecraft.Profiles;
 
@@ -13,11 +12,11 @@ public record UuidProperty(ReadOnlyMemory<byte> Value) : IPacketProperty<UuidPro
 
     public static UuidProperty FromUuid(Uuid value)
     {
-        using var stream = new MemoryStream();
-        var buffer = new MinecraftBuffer(stream);
+        var memory = new byte[16];
+        var buffer = new MinecraftBuffer(memory);
         buffer.WriteUuid(value);
 
-        return new UuidProperty(stream.ToArray());
+        return new UuidProperty(memory);
     }
 
     public static UuidProperty Read(ref MinecraftBuffer buffer)
