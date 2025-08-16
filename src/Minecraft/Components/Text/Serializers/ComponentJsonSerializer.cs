2477dfeab3369c4600aadac020dad3e2499ae2c8
caunt <gpg@caunt.world>
2025-08-16 21:52:18 +0300

    fix(components): 🐛 improve JSON deserialization logic
    Refactor the deserialization process to eliminate unnecessary error handling. The method now directly parses the JSON node and returns a default component if parsing fails, enhancing reliability and performance.

--- src/Minecraft/Components/Text/Serializers/ComponentJsonSerializer.cs
+++ src/Minecraft/Components/Text/Serializers/ComponentJsonSerializer.cs
@@ -1,5 +1,4 @@
-﻿using System.Text.Json;
-using System.Text.Json.Nodes;
+﻿using System.Text.Json.Nodes;
 using Void.Minecraft.Nbt.Serializers.Json;
 
 namespace Void.Minecraft.Components.Text.Serializers;
@@ -14,21 +13,8 @@ public static class ComponentJsonSerializer
 
     public static Component Deserialize(string value)
     {
-        var node = (JsonNode?)null;
-
-        try
-        {
-            node = JsonNode.Parse(value);
-        }
-        catch (JsonException)
-        {
-            // ignore, not a json
-        }
-
-        if (node is null)
-            return Deserialize(value);
-        else
-            return Deserialize(node);
+        var node = JsonNode.Parse(value);
+        return node is null ? Component.Default : Deserialize(node);
     }
 
     public static Component Deserialize(JsonNode node)
