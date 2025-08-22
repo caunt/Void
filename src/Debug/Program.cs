7f28900248807c733db1befb5bea22e45fe58119
caunt <gpg@caunt.world>
2025-08-23 01:49:14 +0300

    chore(docker): ✨ add conditional Docker environment startup
    Introduce a new boolean variable `paper` to control the execution flow. The program can now either start a Docker environment directly or run an entry point with specific arguments based on the value of `paper`. This enhances flexibility in managing the Docker environment startup process.

--- src/Debug/Program.cs
+++ src/Debug/Program.cs
@@ -11,21 +11,30 @@ if (OperatingSystem.IsWindows())
 
 var version = ProtocolVersion.Latest;
 var count = 1;
+var paper = true;
 
 if (args.Length is 1 && int.TryParse(args[0], out var value))
     count = value;
 
 Console.WriteLine(@$"Starting {count} minecraft container(s)");
 
-await StartDockerEnvironmentAsync(version, count);
-// await EntryPoint.RunAsync(new EntryPoint.RunOptions { Arguments = [
-//         "--ignore-file-servers",
-//         "--port", "25565",
-//         "--server", "127.0.0.1:25566",
-//         "--server", "127.0.0.1:25567",
-//         "--server", "127.0.0.1:25568"
-//     ]
-// });
+if (paper)
+{
+    await StartDockerEnvironmentAsync(version, count);
+}
+else
+{
+    await EntryPoint.RunAsync(new EntryPoint.RunOptions
+    {
+        Arguments = [
+            "--ignore-file-servers",
+            "--port", "25565",
+            "--server", "127.0.0.1:25566",
+            "--server", "127.0.0.1:25567",
+            "--server", "127.0.0.1:25568"
+        ]
+    });
+}
 
 return;
 
