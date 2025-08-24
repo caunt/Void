36944b56738c6f7dc8b27b1f8dc4d1b10e5de8c7
caunt <gpg@caunt.world>
2025-08-25 01:53:44 +0300

    feat(console): ✨ allow passing command-line options to plugins
    Updated the interface to improve command-line option retrieval capabilities. Added methods for safely accessing option values and clarified existing method documentation. The project now includes a dependency on the System.CommandLine library for enhanced command-line parsing functionality.

--- src/Api/Console/IConsoleService.cs
+++ src/Api/Console/IConsoleService.cs
@@ -1,4 +1,6 @@
-﻿using Void.Proxy.Api.Commands;
+﻿using System.CommandLine;
+using System.Diagnostics.CodeAnalysis;
+using Void.Proxy.Api.Commands;
 
 namespace Void.Proxy.Api.Console;
 
@@ -10,10 +12,39 @@ public interface IConsoleService : ICommandSource
     public void Setup();
 
     /// <summary>
-    /// Asynchronously processes single incoming command until cancellation is requested.
+    /// Asynchronously processes single incoming command in the terminal until cancellation is requested.
     /// </summary>
     /// <param name="cancellationToken">A token to monitor for cancellation requests. The operation will stop processing current command if the token is
     /// canceled.</param>
     /// <returns>A <see cref="ValueTask"/> representing the asynchronous operation.</returns>
     public ValueTask HandleCommandsAsync(CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Attempts to retrieve the value associated with the specified option from the parsed command-line arguments.
+    /// </summary>
+    /// <typeparam name="TValue">The type of the value associated with the option.</typeparam>
+    /// <param name="option">The option for which to retrieve the value. Cannot be <see langword="null"/>.</param>
+    /// <param name="value">When this method returns <see langword="true"/>, contains the value associated with the specified option. When
+    /// this method returns <see langword="false"/>, the value is <see langword="null"/>.</param>
+    /// <returns><see langword="true"/> if the value for the specified option was successfully retrieved;  otherwise, <see
+    /// langword="false"/>.</returns>
+    public bool TryGetOptionValue<TValue>(Option<TValue> option, [MaybeNullWhen(false)] out TValue value);
+
+    /// <summary>
+    /// Retrieves the value associated with the specified option from the parsed command-line arguments.
+    /// </summary>
+    /// <remarks>Use this method to access the value of a specific option after parsing command-line
+    /// arguments. If the option is not present in the arguments, the method returns <see langword="null"/>.</remarks>
+    /// <typeparam name="TValue">The type of the value associated with the option.</typeparam>
+    /// <param name="option">The option whose value is to be retrieved. Cannot be <see langword="null"/>.</param>
+    /// <returns>The value of the specified option if it is present; otherwise, <see langword="null"/>.</returns>
+    public TValue? GetOptionValue<TValue>(Option<TValue> option);
+
+    /// <summary>
+    /// Retrieves the value of the specified option, ensuring that it is set from the parsed command-line arguments.
+    /// </summary>
+    /// <typeparam name="TValue">The type of the value associated with the option.</typeparam>
+    /// <param name="option">The option whose value is to be retrieved. Cannot be <see langword="null"/>.</param>
+    /// <returns>The value of the specified option.</returns>
+    public TValue GetRequiredOptionValue<TValue>(Option<TValue> option);
 }
