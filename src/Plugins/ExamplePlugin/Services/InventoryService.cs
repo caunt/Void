d9f046f53bb2adb13f26b8d6408426cbe84b4b69
Vitalii Kotenko <caunt.official@gmail.com>
2025-10-15 09:38:11 +0300

    style(plugins): ✏️ clarify phase description comment
    No observable behavior changes; fixed typo in explanatory comment.

--- src/Plugins/ExamplePlugin/Services/InventoryService.cs
+++ src/Plugins/ExamplePlugin/Services/InventoryService.cs
@@ -34,7 +34,7 @@ public class InventoryService(IPlayerContext context, ILogger<InventoryService> 
     [Subscribe]
     public void OnPhaseChanged(PhaseChangedEvent @event)
     {
-        // Minecraft Phase indicate state of the game. Common Phases are Handshake, Login, Configuration and Play.
+        // Minecraft phases indicate the state of the game. Common phases are Handshake, Login, Configuration and Play.
         // They are NOT synced between server and player instantly. When player is in Play phase, server might be still in Login phase.
         // This means you should decide which side Phase change you want to handle here. In this case, both sides are handled.
 
