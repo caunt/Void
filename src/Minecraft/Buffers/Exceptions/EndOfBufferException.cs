2a96fb5da5ffa1841cc1187fc26a2a79929d8a6c
caunt <gpg@caunt.world>
2026-01-03 03:25:29 +0200

    feat(buffers): ✨ added support for large buffer indices
    Refactored buffer exception handling to use long for position, length, and size, enabling support for larger buffers. Added overloads for multiple numeric types to improve compatibility and flexibility with various buffer index types.

--- src/Minecraft/Buffers/Exceptions/EndOfBufferException.cs
+++ src/Minecraft/Buffers/Exceptions/EndOfBufferException.cs
@@ -8,9 +8,29 @@ namespace Void.Minecraft.Buffers.Exceptions;
 /// <param name="position">Specifies the starting point of the attempted access within the buffer.</param>
 /// <param name="length">Indicates the number of bytes that were attempted to be accessed.</param>
 /// <param name="size">Represents the total size of the buffer available for access.</param>
-public class EndOfBufferException(int position, int length, int size) : Exception($"You tried to access {length} bytes from the buffer, but only {size - position} bytes are available ({position}/{size}).")
+public class EndOfBufferException(long position, long length, long size) : Exception($"You tried to access {length} bytes from the buffer, but only {size - position} bytes are available ({position}/{size}).")
 {
-    public int Position { get; } = position;
-    public int Length { get; } = length;
-    public int Size { get; } = size;
+    public long Position { get; } = position;
+    public long Length { get; } = length;
+    public long Size { get; } = size;
+
+    public EndOfBufferException(int position, int length, int size) : this((long)position, length, size)
+    {
+        // Intentionally left blank
+    }
+
+    public EndOfBufferException(nint position, nint length, nint size) : this((long)position, length, size)
+    {
+        // Intentionally left blank
+    }
+
+    public EndOfBufferException(short position, short length, short size) : this((long)position, length, size)
+    {
+        // Intentionally left blank
+    }
+
+    public EndOfBufferException(byte position, byte length, byte size) : this((long)position, length, size)
+    {
+        // Intentionally left blank
+    }
 }
