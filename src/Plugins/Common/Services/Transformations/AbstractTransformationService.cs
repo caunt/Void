03f3cdb239adee42f7891e250505723b1d1632e5
caunt <gpg@caunt.world>
2026-01-04 06:25:50 +0200

    refactor(transformations): ♻️ centralized transformations registrations
    Refactor transformation services to centralize event subscription and protocol version checks in the base service. This reduces code duplication and makes it easier to maintain and extend protocol support for Minecraft versions. Concrete services now only specify supported versions.

--- src/Plugins/Common/Services/Transformations/AbstractTransformationService.cs
+++ src/Plugins/Common/Services/Transformations/AbstractTransformationService.cs
@@ -1,3 +1,26 @@
-﻿namespace Void.Proxy.Plugins.Common.Services.Transformations;
+﻿using Void.Minecraft.Events;
+using Void.Minecraft.Network;
+using Void.Minecraft.Players.Extensions;
+using Void.Proxy.Api.Events;
+using Void.Proxy.Api.Network;
+using Void.Proxy.Plugins.Common.Network.Packets.Transformations;
 
-public abstract class AbstractTransformationService : IPluginCommonService;
+namespace Void.Proxy.Plugins.Common.Services.Transformations;
+
+public abstract class AbstractTransformationService : IPluginCommonService
+{
+
+    [Subscribe(PostOrder.First)]
+    public void OnPhaseChanged(PhaseChangedEvent @event)
+    {
+        if (!IsSupportedVersion(@event.Player.ProtocolVersion))
+            return;
+
+        if (@event is not { Side: Side.Server, Phase: Phase.Play })
+            return;
+
+        NetworkTransformations.Register(@event.Player);
+    }
+
+    protected abstract bool IsSupportedVersion(ProtocolVersion protocolVersion);
+}
--- src/Plugins/ProtocolSupport/Java/v1_13_to_1_20_1/Transformations/TransformationService.cs
+++ src/Plugins/ProtocolSupport/Java/v1_13_to_1_20_1/Transformations/TransformationService.cs
@@ -1,24 +1,12 @@
-﻿using Void.Minecraft.Events;
-using Void.Minecraft.Network;
-using Void.Minecraft.Players.Extensions;
-using Void.Proxy.Api.Events;
-using Void.Proxy.Api.Network;
-using Void.Proxy.Plugins.Common.Network.Packets.Transformations;
+﻿using Void.Minecraft.Network;
 using Void.Proxy.Plugins.Common.Services.Transformations;
 
 namespace Void.Proxy.Plugins.ProtocolSupport.Java.v1_13_to_1_20_1.Transformations;
 
 public class TransformationService : AbstractTransformationService
 {
-    [Subscribe(PostOrder.First)]
-    public static void OnPhaseChanged(PhaseChangedEvent @event)
+    protected override bool IsSupportedVersion(ProtocolVersion protocolVersion)
     {
-        if (!Plugin.SupportedVersions.Contains(@event.Player.ProtocolVersion))
-            return;
-
-        if (@event is not { Side: Side.Server, Phase: Phase.Play })
-            return;
-
-        NetworkTransformations.Register(@event.Player);
+        return Plugin.SupportedVersions.Contains(protocolVersion);
     }
 }
