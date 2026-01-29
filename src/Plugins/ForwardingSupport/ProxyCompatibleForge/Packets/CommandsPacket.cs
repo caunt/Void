e0020944181ed21cbc4326dcd8c15a629e47bc07
caunt <gpg@caunt.world>
2026-01-29 08:05:36 +0200

    feat(proxy): ✨ added support for CommandsPacket handling
    Introduce CommandsPacket for raw command data encoding/decoding and register it during Play phase. Add event handler to log received CommandsPacket data length using injected logger. Refactor service to require logger via constructor. Clean up using directives.

---
+++ src/Plugins/ForwardingSupport/ProxyCompatibleForge/Packets/CommandsPacket.cs
@@ -0,0 +1,28 @@
+﻿using Void.Minecraft.Buffers;
+using Void.Minecraft.Network;
+using Void.Minecraft.Network.Messages.Packets;
+
+namespace Void.Proxy.Plugins.ForwardingSupport.ProxyCompatibleForge.Packets;
+
+public class CommandsPacket : IMinecraftClientboundPacket<CommandsPacket>
+{
+    public required byte[] Data { get; set; }
+
+    public void Encode(ref MinecraftBuffer buffer, ProtocolVersion protocolVersion)
+    {
+        buffer.Write(Data);
+    }
+
+    public static CommandsPacket Decode(ref MinecraftBuffer buffer, ProtocolVersion protocolVersion)
+    {
+        return new CommandsPacket
+        {
+            Data = buffer.ReadToEnd().ToArray()
+        };
+    }
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+    }
+}
--- src/Plugins/ForwardingSupport/ProxyCompatibleForge/Services/CompatibilityService.cs
+++ src/Plugins/ForwardingSupport/ProxyCompatibleForge/Services/CompatibilityService.cs
@@ -1,7 +1,40 @@
-﻿using Void.Proxy.Api.Events;
+﻿using Microsoft.Extensions.Logging;
+using Void.Minecraft.Events;
+using Void.Minecraft.Network;
+using Void.Minecraft.Players.Extensions;
+using Void.Proxy.Api.Events;
+using Void.Proxy.Api.Events.Network;
+using Void.Proxy.Api.Network;
+using Void.Proxy.Plugins.ForwardingSupport.ProxyCompatibleForge.Packets;
 
 namespace Void.Proxy.Plugins.ForwardingSupport.ProxyCompatibleForge.Services;
 
-public class CompatibilityService() : IEventListener
+public class CompatibilityService(ILogger<CompatibilityService> logger) : IEventListener
 {
+    [Subscribe]
+    public static void OnPhaseChanged(PhaseChangedEvent @event)
+    {
+        if (@event.Phase is not Phase.Play)
+            return;
+
+        @event.Player.RegisterPacket<CommandsPacket>(
+            @event.Channel,
+            @event.Side switch
+            {
+                Side.Client => Operation.Write,
+                Side.Server => Operation.Read,
+                _ => throw new InvalidOperationException($"Invalid side changed phase: {@event.Side}")
+            },
+            PacketIdDefinitions.ClientboundCommands);
+    }
+
+    [Subscribe]
+    public void OnMessageReceived(MessageReceivedEvent @event)
+    {
+        if (@event.Message is not CommandsPacket commandsPacket)
+            return;
+
+        Console.WriteLine(@event);
+        logger.LogDebug("Received CommandsPacket: DataLength={DataLength}", commandsPacket.Data.Length);
+    }
 }
