c22507265afc7a7c8a67f408f943de6466fab583
caunt <gpg@caunt.world>
2026-01-29 08:33:28 +0200

    feat(network): ✨ updated identifier type and interface implementation
    Changed identifier from string to ArgumentIdentifier in passthrough property. Now explicitly throws NotSupportedException for Examples and Parse, reflecting unsupported operations.

--- src/Minecraft/Commands/Brigadier/Network/Arguments/PassthroughProperty.cs
+++ src/Minecraft/Commands/Brigadier/Network/Arguments/PassthroughProperty.cs
@@ -5,7 +5,7 @@ using Void.Minecraft.Commands.Brigadier.Network.Serializers;
 
 namespace Void.Minecraft.Commands.Brigadier.Network.Arguments;
 
-public record PassthroughProperty<T>(string Identifier, IArgumentPropertySerializer<T> Serializer, T Result) : IArgumentType<T>
+public record PassthroughProperty<T>(ArgumentIdentifier Identifier, IArgumentPropertySerializer<T> Serializer, T Result) : IArgumentType<T>
 {
     public IEnumerable<string> Examples => throw new NotSupportedException();
 
