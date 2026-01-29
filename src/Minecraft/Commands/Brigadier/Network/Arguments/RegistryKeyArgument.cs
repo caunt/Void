071bb9a83aa1075e81ac826e97c6420ae83a7eb5
caunt <gpg@caunt.world>
2026-01-29 08:04:03 +0200

    feat(brigadier): ✨ added support for mod and registry key args
    Introduce support for mod argument properties and registry key arguments in the Brigadier command system. This adds new argument types for handling mod-specific data and registry key parsing, enabling broader extensibility for custom commands.

---
+++ src/Minecraft/Commands/Brigadier/Network/Arguments/RegistryKeyArgument.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Void.Minecraft.Commands.Brigadier.ArgumentTypes;
+
+namespace Void.Minecraft.Commands.Brigadier.Network.Arguments;
+
+public class RegistryKeyArgument(string identifier) : IArgumentType<string>
+{
+    public string Identifier => identifier;
+    public IEnumerable<string> Examples => ["foo", "foo:bar", "012"];
+
+    public string Parse(StringReader reader)
+    {
+        return reader.ReadString();
+    }
+}
