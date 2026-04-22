8c0d55a8f9323d705413ac0507948e361b33ed70
Vitalii Kotenko <caunt.official@gmail.com>
2026-04-22 13:48:07 +0300

    Update astro.config.mjs

--- docs/astro/astro.config.mjs
+++ docs/astro/astro.config.mjs
@@ -15,7 +15,7 @@ const googleAnalyticsId = 'G-3KT5D46L8T'
 export default defineConfig({
     site: 'https://void.caunt.world',
     integrations: [starlight({
-        title: 'Void',
+        title: 'Void – Minecraft Proxy for Modded Servers',
         social: [
             { icon: 'telegram', label: 'Telegram', href: 'https://t.me/mcVoidProxy' },
             { icon: 'github', label: 'GitHub', href: 'https://github.com/caunt/void' }
