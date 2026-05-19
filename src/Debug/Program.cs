c9898ef95615322f349af99ed32ec344854bc420
Vitalii Kotenko <caunt.official@gmail.com>
2026-05-19 09:48:04 +0300

    refactor(program): 🔧 removed unused parameters and cleaned up command options

--- src/Debug/Program.cs
+++ src/Debug/Program.cs
@@ -45,13 +45,7 @@ string[] arguments = [
     "--override", "s2=args-server-2",
     "--override", "s3=args-server-3",
     "--override", "s4=args-server-4",
-    "--offline",
-    
-    "-r", "http://minecraft.bagetter.svc.cluster.local:80/v3/index.json",
-    "-p", "https://github.com/Shonz1/Void.Plugins/releases/latest/download/void-tab-lists.dll",
-    "-p", "https://github.com/Shonz1/Void.Plugins/releases/latest/download/void-player-positions.dll",
-    "-p", "https://github.com/Shonz1/Void.Plugins/releases/latest/download/void-menus.dll",
-    "-p", "https://pub-637ef13bcc21413ab47c77dd6e1399cf.r2.dev/ExamplePlugin.dll"
+    "--offline"
 ];
 
 Environment.SetEnvironmentVariable("MINECRAFT_API_BASE_URL", "http://minecraft.api.svc.cluster.local:80/");
