1d2b5ca5c7223b867aaafde2a0ba756db94b84f5
caunt <git@caunt.world>
2026-05-28 22:47:14 +0300

    ci(tests): 🔧 adjusted parallel thread settings for GitHub Actions Observable behavior: modified test execution configuration based on environment.

--- tests/Void.IntegrationTests/AssemblyInfo.cs
+++ tests/Void.IntegrationTests/AssemblyInfo.cs
@@ -5,6 +5,11 @@ using Xunit;
 [assembly: CaptureConsole]
 #endif
 
+#if GITHUB_ACTIONS
+[assembly: CollectionBehavior(MaxParallelThreads = 1)]
+#else
 [assembly: CollectionBehavior(MaxParallelThreads = 3)]
+#endif
+
 [assembly: AssemblyFixture(typeof(PaperFixture))]
 [assembly: AssemblyFixture(typeof(PortableMinecraftClientImageFixture))]
